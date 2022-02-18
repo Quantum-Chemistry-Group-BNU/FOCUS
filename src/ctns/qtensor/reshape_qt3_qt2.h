c37fe7552bb180ab1857690d5805716b0c476512
zhendongli <zhendongli2008@gmail.com>
2022-02-18 21:45:09 +0800

    reshape

--- src/ctns/qtensor/reshape_qt3_qt2.h
+++ src/ctns/qtensor/reshape_qt3_qt2.h
@@ -18,33 +18,34 @@ stensor2<Tm> merge_qt3_qt2_lc(const stensor3<Tm>& qt3,
    assert(qt3.dir_row() == qt3.dir_mid()); 
    std::vector<bool> dir = {qt3.dir_row(),qt3.dir_col()};
    stensor2<Tm> qt2(qt3.info.sym, qlc, qcol, dir);
-   for(int bc=0; bc<qt3.cols(); bc++){
-      int cdim = qt3.info.qcol.get_dim(bc);
-      for(int blc=0; blc<qlc.size(); blc++){
-	 auto& blk2 = qt2(blc,bc);
-	 if(blk2.size() == 0) continue;
-	 // loop over compatible l*c = lc
-	 auto qsym_lc = qlc.get_sym(blc);
-         for(const auto& p12 : dpt.at(qsym_lc)){
-	    int br = std::get<0>(p12);
-	    int bm = std::get<1>(p12);
-	    int ioff = std::get<2>(p12);
-	    int rdim = qt3.info.qrow.get_dim(br);
-	    int mdim = qt3.info.qmid.get_dim(bm);
-	    const auto& blk3 = qt3(br,bc,bm);
-	    if(blk3.size() == 0) continue;
-	    for(int im=0; im<mdim; im++){
-	       for(int ic=0; ic<cdim; ic++){
-	          for(int ir=0; ir<rdim; ir++){
-		     // qt3[l,r,c] -> qt2[lc,r]: storage l[fast]*c!
-	             int irm = ioff+im*rdim+ir;
-	             blk2(irm,ic) = blk3(ir,ic,im);
-	          } // ir
-	       } // ic
-	    } // im
-         } // p12			 
-      } // blc
-   } // bc
+   // loop over qt2
+   int blc, bc;
+   for(int i=0; i<qt2.info._nnzaddr.size(); i++){
+      int idx = qt2.info._nnzaddr[i];
+      qt2.info._addr_unpack(idx,blc,bc);
+      auto& blk2 = qt2(blc,bc);
+      // loop over compatible l*c = lc
+      auto qsym_lc = qlc.get_sym(blc);
+      for(const auto& p12 : dpt.at(qsym_lc)){
+	 int br = std::get<0>(p12);
+	 int bm = std::get<1>(p12);
+	 int ioff = std::get<2>(p12);
+	 const auto& blk3 = qt3(br,bc,bm);
+	 if(blk3.size() == 0) continue;
+	 int rdim = blk3.dim0;
+	 int cdim = blk3.dim1;
+	 int mdim = blk3.dim2;
+	 for(int im=0; im<mdim; im++){
+	    for(int ic=0; ic<cdim; ic++){
+	       for(int ir=0; ir<rdim; ir++){
+	          // qt3[l,r,c] -> qt2[lc,r]: storage l[fast]*c!
+	          int irm = ioff+im*rdim+ir;
+	          blk2(irm,ic) = blk3(ir,ic,im);
+	       } // ir
+	    } // ic
+	 } // im
+      } // p12			 
+   } // i
    return qt2;
 }
 // psi3[l,r,c] <- psi2[lc,r]
@@ -57,33 +58,33 @@ stensor3<Tm> split_qt3_qt2_lc(const stensor2<Tm>& qt2,
    const auto& qcol = qt2.info.qcol;
    std::vector<bool> dir = {qt2.dir_row(),qt2.dir_col(),qt2.dir_row()};
    stensor3<Tm> qt3(qt2.info.sym, qlx, qcol, qcx, dir);
-   for(int bc=0; bc<qt3.cols(); bc++){
-      int cdim = qt3.info.qcol.get_dim(bc);
-      for(int blc=0; blc<qlc.size(); blc++){
-	 const auto& blk2 = qt2(blc,bc);
-	 if(blk2.size() == 0) continue;
-	 // loop over compatible l*c = lc
-	 auto qsym_lc = qlc.get_sym(blc);
-         for(const auto& p12 : dpt.at(qsym_lc)){
-	    int br = std::get<0>(p12);
-	    int bm = std::get<1>(p12);
-	    int ioff = std::get<2>(p12);
-	    int rdim = qt3.info.qrow.get_dim(br);
-	    int mdim = qt3.info.qmid.get_dim(bm);
-	    auto& blk3 = qt3(br,bc,bm);
-	    if(blk3.size() == 0) continue;
-	    for(int im=0; im<mdim; im++){
-	       for(int ic=0; ic<cdim; ic++){
-	          for(int ir=0; ir<rdim; ir++){
-		     // qt2[lc,r] -> qt3[l,r,c], storage l[fast]c
-	             int irm = ioff+im*rdim+ir;
-	             blk3(ir,ic,im) = blk2(irm,ic); 
-	          } // ir
-	       } // ic
-	    } // im
-         } // p12			 
-      } // blc
-   } // bc
+   int blc, bc;
+   for(int i=0; i<qt2.info._nnzaddr.size(); i++){
+      int idx = qt2.info._nnzaddr[i];
+      qt2.info._addr_unpack(idx,blc,bc);
+      const auto& blk2 = qt2(blc,bc);
+      // loop over compatible l*c = lc
+      auto qsym_lc = qlc.get_sym(blc);
+      for(const auto& p12 : dpt.at(qsym_lc)){
+	 int br = std::get<0>(p12);
+	 int bm = std::get<1>(p12);
+	 int ioff = std::get<2>(p12);
+	 auto& blk3 = qt3(br,bc,bm);
+	 if(blk3.size() == 0) continue;
+	 int rdim = blk3.dim0;
+	 int cdim = blk3.dim1;
+	 int mdim = blk3.dim2;
+	 for(int im=0; im<mdim; im++){
+	    for(int ic=0; ic<cdim; ic++){
+	       for(int ir=0; ir<rdim; ir++){
+	          // qt2[lc,r] -> qt3[l,r,c], storage l[fast]c
+	          int irm = ioff+im*rdim+ir;
+	          blk3(ir,ic,im) = blk2(irm,ic); 
+	       } // ir
+	    } // ic
+	 } // im
+      } // p12			 
+   } // i
    return qt3;
 }
 
@@ -96,33 +97,34 @@ stensor2<Tm> merge_qt3_qt2_cr(const stensor3<Tm>& qt3,
    assert(qt3.dir_mid() == qt3.dir_col());
    std::vector<bool> dir = {qt3.dir_row(),qt3.dir_mid()};
    stensor2<Tm> qt2(qt3.info.sym, qrow, qcr, dir);
-   for(int br=0; br<qt3.rows(); br++){
-      int rdim = qt3.info.qrow.get_dim(br);
-      for(int bcr=0; bcr<qcr.size(); bcr++){
-	 auto& blk2 = qt2(br,bcr);
-	 if(blk2.size() == 0) continue;
-	 // loop over compatible c*r = cr
-	 auto qsym_cr = qcr.get_sym(bcr);
-	 for(const auto& p12 : dpt.at(qsym_cr)){
-	    int bm = std::get<0>(p12);
-	    int bc = std::get<1>(p12);
-	    int ioff = std::get<2>(p12); 
-	    int mdim = qt3.info.qmid.get_dim(bm);
-	    int cdim = qt3.info.qcol.get_dim(bc);
-	    const auto& blk3 = qt3(br,bc,bm);
-	    if(blk3.size() == 0) continue;
-	    for(int ic=0; ic<cdim; ic++){
-	       for(int im=0; im<mdim; im++){
-		  // qt3[l,r,c] -> qt2[l,cr], storage c[fast]r
-	          int imc = ioff+ic*mdim+im;
-	          for(int ir=0; ir<rdim; ir++){
-	             blk2(ir,imc) = blk3(ir,ic,im); 
-	          } // ir
-	       } // ic
-	    } // im
-	 } // p12 
-      } // bcr 
-   } // br
+   // loop over qt2
+   int br, bcr;
+   for(int i=0; i<qt2.info._nnzaddr.size(); i++){
+      int idx = qt2.info._nnzaddr[i];
+      qt2.info._addr_unpack(idx,br,bcr);
+      auto& blk2 = qt2(br,bcr);
+      // loop over compatible c*r = cr
+      auto qsym_cr = qcr.get_sym(bcr);
+      for(const auto& p12 : dpt.at(qsym_cr)){
+	 int bm = std::get<0>(p12);
+	 int bc = std::get<1>(p12);
+	 int ioff = std::get<2>(p12); 
+	 const auto& blk3 = qt3(br,bc,bm);
+	 if(blk3.size() == 0) continue;
+	 int rdim = blk3.dim0;
+	 int cdim = blk3.dim1;
+	 int mdim = blk3.dim2;
+	 for(int ic=0; ic<cdim; ic++){
+	    for(int im=0; im<mdim; im++){
+	       // qt3[l,r,c] -> qt2[l,cr], storage c[fast]r
+	       int imc = ioff+ic*mdim+im;
+	       for(int ir=0; ir<rdim; ir++){
+	          blk2(ir,imc) = blk3(ir,ic,im); 
+	       } // ir
+	    } // ic
+	 } // im
+      } // p12 
+   } // i
    return qt2;
 }
 // psi3[l,r,c] <- psi2[l,cr]
@@ -135,33 +137,33 @@ stensor3<Tm> split_qt3_qt2_cr(const stensor2<Tm>& qt2,
    const auto& qcr  = qt2.info.qcol;
    std::vector<bool> dir = {qt2.dir_row(),qt2.dir_col(),qt2.dir_col()};
    stensor3<Tm> qt3(qt2.info.sym, qrow, qrx, qcx, dir);
-   for(int br=0; br<qt3.rows(); br++){
-      int rdim = qt3.info.qrow.get_dim(br);
-      for(int bcr=0; bcr<qcr.size(); bcr++){
-	 const auto& blk2 = qt2(br,bcr);
-	 if(blk2.size() == 0) continue;
-	 // loop over compatible c*r = cr
-	 auto qsym_cr = qcr.get_sym(bcr);
-	 for(const auto& p12 : dpt.at(qsym_cr)){
-	    int bm = std::get<0>(p12);
-	    int bc = std::get<1>(p12);
-	    int ioff = std::get<2>(p12); 
-	    int mdim = qt3.info.qmid.get_dim(bm);
-	    int cdim = qt3.info.qcol.get_dim(bc);
-	    auto& blk3 = qt3(br,bc,bm);
-	    if(blk3.size() == 0) continue;
-	    for(int ic=0; ic<cdim; ic++){
-	       for(int im=0; im<mdim; im++){
-		  // qt2[l,cr] -> qt3[l,r,c], storage c[fast]r
-	          int imc = ioff+ic*mdim+im;
-	          for(int ir=0; ir<rdim; ir++){
-		     blk3(ir,ic,im) = blk2(ir,imc); 
-	          } // ir
-	       } // ic
-	    } // im
-	 } // p12 
-      } // bcr 
-   } // br
+   int br, bcr;
+   for(int i=0; i<qt2.info._nnzaddr.size(); i++){
+      int idx = qt2.info._nnzaddr[i];
+      qt2.info._addr_unpack(idx,br,bcr);
+      const auto& blk2 = qt2(br,bcr);
+      // loop over compatible c*r = cr
+      auto qsym_cr = qcr.get_sym(bcr);
+      for(const auto& p12 : dpt.at(qsym_cr)){
+	 int bm = std::get<0>(p12);
+	 int bc = std::get<1>(p12);
+	 int ioff = std::get<2>(p12); 
+	 auto& blk3 = qt3(br,bc,bm);
+	 if(blk3.size() == 0) continue;
+	 int rdim = blk3.dim0;
+	 int cdim = blk3.dim1;
+	 int mdim = blk3.dim2;
+	 for(int ic=0; ic<cdim; ic++){
+	    for(int im=0; im<mdim; im++){
+	       // qt2[l,cr] -> qt3[l,r,c], storage c[fast]r
+	       int imc = ioff+ic*mdim+im;
+	       for(int ir=0; ir<rdim; ir++){
+	          blk3(ir,ic,im) = blk2(ir,imc); 
+	       } // ir
+	    } // ic
+	 } // im
+      } // p12 
+   } // i
    return qt3;
 }
 
@@ -174,33 +176,34 @@ stensor2<Tm> merge_qt3_qt2_lr(const stensor3<Tm>& qt3,
    assert(qt3.dir_row() == qt3.dir_col()); 
    std::vector<bool> dir = {qt3.dir_row(),qt3.dir_mid()};
    stensor2<Tm> qt2(qt3.info.sym, qlr, qmid, dir);
-   for(int bm=0; bm<qt3.mids(); bm++){
-      int mdim = qt3.info.qmid.get_dim(bm);
-      for(int blr=0; blr<qlr.size(); blr++){
-	 auto& blk2 = qt2(blr,bm);
-	 if(blk2.size() == 0) continue;
-	 // loop over comptabile l*r = lr
-	 auto qsym_lr = qlr.get_sym(blr);
-	 for(const auto& p12 : dpt.at(qsym_lr)){
-	    int br = std::get<0>(p12);
-	    int bc = std::get<1>(p12);
-	    int ioff = std::get<2>(p12);
-            int rdim = qt3.info.qrow.get_dim(br);
-	    int cdim = qt3.info.qcol.get_dim(bc);
-	    const auto& blk3 = qt3(br,bc,bm);
-	    if(blk3.size() == 0) continue;
-	       for(int im=0; im<mdim; im++){
-	          for(int ic=0; ic<cdim; ic++){
-		     for(int ir=0; ir<rdim; ir++){
-			// qt3[l,r,c] -> qt2[lr,c], storage l[fast]r
-		        int irc = ioff+ic*rdim+ir;
-		        blk2(irc,im) = blk3(ir,ic,im);
-		     } // ir
-		  } // ic
-	       } // im
-	 } // p12
-      } // blr
-   } // bm
+   // loop over qt2
+   int blr, bm;
+   for(int i=0; i<qt2.info._nnzaddr.size(); i++){
+      int idx = qt2.info._nnzaddr[i];
+      qt2.info._addr_unpack(idx,blr,bm);
+      auto& blk2 = qt2(blr,bm);
+      // loop over comptabile l*r = lr
+      auto qsym_lr = qlr.get_sym(blr);
+      for(const auto& p12 : dpt.at(qsym_lr)){
+	 int br = std::get<0>(p12);
+	 int bc = std::get<1>(p12);
+	 int ioff = std::get<2>(p12);
+	 const auto& blk3 = qt3(br,bc,bm);
+	 if(blk3.size() == 0) continue;
+	 int rdim = blk3.dim0;
+	 int cdim = blk3.dim1;
+	 int mdim = blk3.dim2;
+	 for(int im=0; im<mdim; im++){
+	    for(int ic=0; ic<cdim; ic++){
+	       for(int ir=0; ir<rdim; ir++){
+	  	  // qt3[l,r,c] -> qt2[lr,c], storage l[fast]r
+	          int irc = ioff+ic*rdim+ir;
+	          blk2(irc,im) = blk3(ir,ic,im);
+	       } // ir
+	    } // ic
+	 } // im
+      } // p12
+   } // i
    return qt2;
 }
 // psi3[l,r,c] <- psi2[lr,c]
@@ -213,33 +216,33 @@ stensor3<Tm> split_qt3_qt2_lr(const stensor2<Tm>& qt2,
    const auto& qmid = qt2.info.qcol;
    std::vector<bool> dir = {qt2.dir_row(),qt2.dir_row(),qt2.dir_col()};
    stensor3<Tm> qt3(qt2.info.sym, qlx, qrx, qmid, dir);
-   for(int bm=0; bm<qt3.mids(); bm++){
-      int mdim = qt3.info.qmid.get_dim(bm);
-      for(int blr=0; blr<qlr.size(); blr++){
-	 const auto& blk2 = qt2(blr,bm);
-	 if(blk2.size() == 0) continue;
-	 // loop over comptabile l*r = lr
-	 auto qsym_lr = qlr.get_sym(blr);
-	 for(const auto& p12 : dpt.at(qsym_lr)){
-	    int br = std::get<0>(p12);
-	    int bc = std::get<1>(p12);
-	    int ioff = std::get<2>(p12);
-            int rdim = qt3.info.qrow.get_dim(br);
-	    int cdim = qt3.info.qcol.get_dim(bc);
-	    auto& blk3 = qt3(br,bc,bm);
-	    if(blk3.size() == 0) continue;
-	       for(int im=0; im<mdim; im++){
-	          for(int ic=0; ic<cdim; ic++){
-		     for(int ir=0; ir<rdim; ir++){
-			// qt3[l,c,r] -> qt2[lr,c], storage l[fast]r
-		        int irc = ioff+ic*rdim+ir;
-		       	blk3(ir,ic,im) = blk2(irc,im);
-		     } // ir
-		  } // ic
-	       } // im
-	 } // p12
-      } // blr
-   } // bm
+   int blr, bm;
+   for(int i=0; i<qt2.info._nnzaddr.size(); i++){
+      int idx = qt2.info._nnzaddr[i];
+      qt2.info._addr_unpack(idx,blr,bm);
+      const auto& blk2 = qt2(blr,bm);
+      // loop over comptabile l*r = lr
+      auto qsym_lr = qlr.get_sym(blr);
+      for(const auto& p12 : dpt.at(qsym_lr)){
+	 int br = std::get<0>(p12);
+	 int bc = std::get<1>(p12);
+	 int ioff = std::get<2>(p12);
+	 auto& blk3 = qt3(br,bc,bm);
+	 if(blk3.size() == 0) continue;
+	 int rdim = blk3.dim0;
+	 int cdim = blk3.dim1;
+	 int mdim = blk3.dim2;
+	 for(int im=0; im<mdim; im++){
+	    for(int ic=0; ic<cdim; ic++){
+	       for(int ir=0; ir<rdim; ir++){
+	          // qt3[l,c,r] -> qt2[lr,c], storage l[fast]r
+	          int irc = ioff+ic*rdim+ir;
+	          blk3(ir,ic,im) = blk2(irc,im);
+	       } // ir
+	    } // ic
+	 } // im
+      } // p12
+   } // i
    return qt3;
 }
 
--- src/ctns/qtensor/reshape_qt4_qt3.h
+++ src/ctns/qtensor/reshape_qt4_qt3.h
@@ -18,38 +18,37 @@ stensor3<Tm> merge_qt4_qt3_lc1(const stensor4<Tm>& qt4,
    const auto& qcol = qt4.info.qcol;
    std::vector<bool> dir = {1,1,1};
    stensor3<Tm> qt3(sym, qlc1, qcol, qver, dir);
-   for(int bv=0; bv<qver.size(); bv++){
-      int vdim = qver.get_dim(bv);
-      for(int bc=0; bc<qcol.size(); bc++){
-         int cdim = qcol.get_dim(bc);
-         for(int blc1=0; blc1<qlc1.size(); blc1++){
-	    auto& blk3 = qt3(blc1,bc,bv);
-	    if(blk3.size() == 0) continue;
-	    // loop over compatible l*c1 = lc1
-	    auto qsym_lc1 = qlc1.get_sym(blc1);
-	    for(const auto& p12 : dpt.at(qsym_lc1)){
-	       int br = std::get<0>(p12);
-	       int bm = std::get<1>(p12);
-	       int ioff = std::get<2>(p12);
-	       int rdim = qt4.info.qrow.get_dim(br);
-	       int mdim = qt4.info.qmid.get_dim(bm); 
-	       const auto& blk4 = qt4(br,bc,bm,bv);
-	       if(blk4.size() == 0) continue; 
-	       for(int iv=0; iv<vdim; iv++){
-	          for(int im=0; im<mdim; im++){
-	            for(int ic=0; ic<cdim; ic++){
-	               for(int ir=0; ir<rdim; ir++){
-	                  // psi4[l,r,c1,c2] <-> psi3[l(fast)c1,r,c2]
-	                  int irm = ioff+im*rdim+ir; 
-	                  blk3(irm,ic,iv) = blk4(ir,ic,im,iv); // internally c1[fast]c2
-	               } // ir
-	            } // ic
-	          } // im
-	       } // iv
-	    } // p12
-	 } // bc	 
-      } // blc
-   } // bv
+   // loop over qt3
+   int blc1, bc, bv;
+   for(int i=0; i<qt3.info._nnzaddr.size(); i++){
+      int idx = qt3.info._nnzaddr[i];
+      qt3.info._addr_unpack(idx,blc1,bc,bv);
+      auto& blk3 = qt3(blc1,bc,bv);
+      // loop over compatible l*c1 = lc1
+      auto qsym_lc1 = qlc1.get_sym(blc1);
+      for(const auto& p12 : dpt.at(qsym_lc1)){
+         int br = std::get<0>(p12);
+         int bm = std::get<1>(p12);
+         int ioff = std::get<2>(p12);
+         const auto& blk4 = qt4(br,bc,bm,bv);
+         if(blk4.size() == 0) continue; 
+         int rdim = blk4.dim0;
+         int cdim = blk4.dim1;
+         int mdim = blk4.dim2; 
+         int vdim = blk4.dim3; 
+         for(int iv=0; iv<vdim; iv++){
+            for(int im=0; im<mdim; im++){
+               for(int ic=0; ic<cdim; ic++){
+                  for(int ir=0; ir<rdim; ir++){
+                     // psi4[l,r,c1,c2] <-> psi3[l(fast)c1,r,c2]
+                     int irm = ioff+im*rdim+ir; 
+                     blk3(irm,ic,iv) = blk4(ir,ic,im,iv); // internally c1[fast]c2
+                  } // ir
+               } // ic
+            } // im
+         } // iv
+      } // p12
+   } // i
    return qt3;
 }
 // psi4[l,r,c1,c2] <- psi3[lc1,r,c2]
@@ -63,38 +62,37 @@ stensor4<Tm> split_qt4_qt3_lc1(const stensor3<Tm>& qt3,
    const auto& qver = qt3.info.qmid;
    const auto& qcol = qt3.info.qcol;
    stensor4<Tm> qt4(sym, qlx, qcol, qc1, qver);
-   for(int bv=0; bv<qver.size(); bv++){
-      int vdim = qver.get_dim(bv);
-      for(int bc=0; bc<qcol.size(); bc++){
-         int cdim = qcol.get_dim(bc);
-         for(int blc1=0; blc1<qlc1.size(); blc1++){
-	    const auto& blk3 = qt3(blc1,bc,bv);
-	    if(blk3.size() == 0) continue;
-	    // loop over compatible l*c = lc
-	    auto qsym_lc1 = qlc1.get_sym(blc1);
-	    for(const auto& p12 : dpt.at(qsym_lc1)){
-	       int br = std::get<0>(p12);
-	       int bm = std::get<1>(p12);
-	       int ioff = std::get<2>(p12);
-	       int rdim = qt4.info.qrow.get_dim(br);
-	       int mdim = qt4.info.qmid.get_dim(bm); 
-	       auto& blk4 = qt4(br,bc,bm,bv);
-	       if(blk4.size() == 0) continue; 
-	       for(int iv=0; iv<vdim; iv++){
-	          for(int im=0; im<mdim; im++){
-	            for(int ic=0; ic<cdim; ic++){
-	               for(int ir=0; ir<rdim; ir++){
-	                  // psi4[l,r,c1,c2] <-> psi3[lc1,r,c2]
-	                  int irm = ioff+im*rdim+ir; // store (ir,im) 
-	                  blk4(ir,ic,im,iv) = blk3(irm,ic,iv); // internally c1[fast]c2
-	               } // ir
-	            } // ic
-	          } // im
-	       } // iv
-	    } // p12
-	 } // bc	 
-      } // blc
-   } // bv
+   // loop over qt3
+   int blc1, bc, bv;
+   for(int i=0; i<qt3.info._nnzaddr.size(); i++){
+      int idx = qt3.info._nnzaddr[i];
+      qt3.info._addr_unpack(idx,blc1,bc,bv);
+      const auto& blk3 = qt3(blc1,bc,bv);
+      // loop over compatible l*c = lc
+      auto qsym_lc1 = qlc1.get_sym(blc1);
+      for(const auto& p12 : dpt.at(qsym_lc1)){
+         int br = std::get<0>(p12);
+         int bm = std::get<1>(p12);
+         int ioff = std::get<2>(p12);
+         auto& blk4 = qt4(br,bc,bm,bv);
+         if(blk4.size() == 0) continue; 
+         int rdim = blk4.dim0;
+         int cdim = blk4.dim1;
+         int mdim = blk4.dim2; 
+         int vdim = blk4.dim3; 
+	 for(int iv=0; iv<vdim; iv++){
+	    for(int im=0; im<mdim; im++){
+	      for(int ic=0; ic<cdim; ic++){
+	         for(int ir=0; ir<rdim; ir++){
+	            // psi4[l,r,c1,c2] <-> psi3[lc1,r,c2]
+	            int irm = ioff+im*rdim+ir; // store (ir,im) 
+	            blk4(ir,ic,im,iv) = blk3(irm,ic,iv); // internally c1[fast]c2
+	         } // ir
+	      } // ic
+	    } // im
+	 } // iv
+      } // p12
+   } // i
    return qt4;
 }
 
@@ -108,38 +106,37 @@ stensor3<Tm> merge_qt4_qt3_c2r(const stensor4<Tm>& qt4,
    const auto& qmid = qt4.info.qmid;
    std::vector<bool> dir = {1,1,1};
    stensor3<Tm> qt3(sym, qrow, qc2r, qmid, dir);
-   for(int bm=0; bm<qmid.size(); bm++){
-      int mdim = qmid.get_dim(bm);
-      for(int br=0; br<qrow.size(); br++){
-         int rdim = qrow.get_dim(br);
-         for(int bc2r=0; bc2r<qc2r.size(); bc2r++){
-	    auto& blk3 = qt3(br,bc2r,bm);
-	    if(blk3.size() == 0) continue;
-	    // loop over compatible c2*r = c2r
-	    auto qsym_c2r = qc2r.get_sym(bc2r);
-	    for(const auto& p12 : dpt.at(qsym_c2r)){
-	       int bv = std::get<0>(p12);
-	       int bc = std::get<1>(p12);
-	       int ioff = std::get<2>(p12);
-	       int vdim = qt4.info.qver.get_dim(bv);
-	       int cdim = qt4.info.qcol.get_dim(bc);
-	       const auto& blk4 = qt4(br,bc,bm,bv);
-	       if(blk4.size() == 0) continue;
-	       for(int iv=0; iv<vdim; iv++){
-	          for(int im=0; im<mdim; im++){
-	             for(int ic=0; ic<cdim; ic++){
-		        for(int ir=0; ir<rdim; ir++){
-	                   // psi4[l,r,c1,c2] <-> psi3[l,c2r,c1]
-			   int ivc = ioff+ic*vdim+iv;
-		           blk3(ir,ivc,im) = blk4(ir,ic,im,iv);
-		        } // ir
-		     } // ic
-	          } // im
-	       } // iv
-	    } // p12
-	 } // bc2r
-      } // br
-   } // bm
+   // loop over qt3
+   int br, bc2r, bm;
+   for(int i=0; i<qt3.info._nnzaddr.size(); i++){
+      int idx = qt3.info._nnzaddr[i];
+      qt3.info._addr_unpack(idx,br,bc2r,bm);
+      auto& blk3 = qt3(br,bc2r,bm);
+      // loop over compatible c2*r = c2r
+      auto qsym_c2r = qc2r.get_sym(bc2r);
+      for(const auto& p12 : dpt.at(qsym_c2r)){
+         int bv = std::get<0>(p12);
+         int bc = std::get<1>(p12);
+         int ioff = std::get<2>(p12);
+         const auto& blk4 = qt4(br,bc,bm,bv);
+         if(blk4.size() == 0) continue;
+         int rdim = blk4.dim0;
+         int cdim = blk4.dim1;
+         int mdim = blk4.dim2; 
+         int vdim = blk4.dim3; 
+	 for(int iv=0; iv<vdim; iv++){
+	    for(int im=0; im<mdim; im++){
+	       for(int ic=0; ic<cdim; ic++){
+	          // psi4[l,r,c1,c2] <-> psi3[l,c2r,c1]
+	  	  int ivc = ioff+ic*vdim+iv;
+	          for(int ir=0; ir<rdim; ir++){
+	             blk3(ir,ivc,im) = blk4(ir,ic,im,iv);
+	          } // ir
+	       } // ic
+	    } // im
+	 } // iv
+      } // p12
+   } // i
    return qt3;
 }
 // psi4[l,r,c1,c2] <- psi3[l,c2r,c1]
@@ -153,38 +150,37 @@ stensor4<Tm> split_qt4_qt3_c2r(const stensor3<Tm>& qt3,
    const auto& qmid = qt3.info.qmid;
    const auto& qc2r = qt3.info.qcol;
    stensor4<Tm> qt4(sym, qrow, qrx, qmid, qc2);
-   for(int bm=0; bm<qmid.size(); bm++){
-      int mdim = qmid.get_dim(bm);
-      for(int br=0; br<qrow.size(); br++){
-         int rdim = qrow.get_dim(br);
-         for(int bc2r=0; bc2r<qc2r.size(); bc2r++){
-	    const auto& blk3 = qt3(br,bc2r,bm);
-	    if(blk3.size() == 0) continue;
-	    // loop over compatible c2*r = c2r
-	    auto qsym_c2r = qc2r.get_sym(bc2r);
-	    for(const auto& p12 : dpt.at(qsym_c2r)){
-	       int bv = std::get<0>(p12);
-	       int bc = std::get<1>(p12);
-	       int ioff = std::get<2>(p12);
-	       int vdim = qt4.info.qver.get_dim(bv);
-	       int cdim = qt4.info.qcol.get_dim(bc);
-	       auto& blk4 = qt4(br,bc,bm,bv);
-	       if(blk4.size() == 0) continue;
-	       for(int iv=0; iv<vdim; iv++){
-	          for(int im=0; im<mdim; im++){
-	             for(int ic=0; ic<cdim; ic++){
-		        for(int ir=0; ir<rdim; ir++){
-	                   // psi4[l,r,c1,c2] <-> psi3[l,c2r,c1]
-			   int ivc = ioff+ic*vdim+iv;
-		           blk4(ir,ic,im,iv) = blk3(ir,ivc,im);
-		        } // ir
-		     } // ic
-	          } // im
-	       } // iv
-	    } // p12
-	 } // bc2r
-      } // br
-   } // bm
+   // loop over qt3
+   int br, bc2r, bm;
+   for(int i=0; i<qt3.info._nnzaddr.size(); i++){
+      int idx = qt3.info._nnzaddr[i];
+      qt3.info._addr_unpack(idx,br,bc2r,bm);
+      const auto& blk3 = qt3(br,bc2r,bm);
+      // loop over compatible c2*r = c2r
+      auto qsym_c2r = qc2r.get_sym(bc2r);
+      for(const auto& p12 : dpt.at(qsym_c2r)){
+         int bv = std::get<0>(p12);
+         int bc = std::get<1>(p12);
+         int ioff = std::get<2>(p12);
+         auto& blk4 = qt4(br,bc,bm,bv);
+         if(blk4.size() == 0) continue;
+         int rdim = blk4.dim0;
+         int cdim = blk4.dim1;
+         int mdim = blk4.dim2; 
+         int vdim = blk4.dim3; 
+         for(int iv=0; iv<vdim; iv++){
+            for(int im=0; im<mdim; im++){
+               for(int ic=0; ic<cdim; ic++){
+                  // psi4[l,r,c1,c2] <-> psi3[l,c2r,c1]
+  		  int ivc = ioff+ic*vdim+iv;
+  	          for(int ir=0; ir<rdim; ir++){
+  	             blk4(ir,ic,im,iv) = blk3(ir,ivc,im);
+  	          } // ir
+  	       } // ic
+            } // im
+         } // iv
+      } // p12
+   } // i
    return qt4;
 }
 
@@ -198,38 +194,37 @@ stensor3<Tm> merge_qt4_qt3_c1c2(const stensor4<Tm>& qt4,
    const auto& qcol = qt4.info.qcol;
    std::vector<bool> dir = {1,1,1};
    stensor3<Tm> qt3(sym, qrow, qcol, qc1c2, dir);
-   for(int bc1c2=0; bc1c2<qc1c2.size(); bc1c2++){
-      for(int br=0; br<qrow.size(); br++){
-	 int rdim = qrow.get_dim(br);
-         for(int bc=0; bc<qcol.size(); bc++){
-	    int cdim = qcol.get_dim(bc);
-	    auto& blk3 = qt3(br,bc,bc1c2);
-	    if(blk3.size() == 0) continue;
-	    // lover over compatible c1*c2 = c1c2
-	    auto qsym_c1c2 = qc1c2.get_sym(bc1c2);
-	    for(const auto& p12 : dpt.at(qsym_c1c2)){
-	       int bm = std::get<0>(p12);
-	       int bv = std::get<1>(p12);
-	       int ioff = std::get<2>(p12);
-	       int mdim = qt4.info.qmid.get_dim(bm);
-	       int vdim = qt4.info.qver.get_dim(bv);
-	       const auto& blk4 = qt4(br,bc,bm,bv);
-	       if(blk4.size() == 0) continue;
-	       for(int iv=0; iv<vdim; iv++){
-		  for(int im=0; im<mdim; im++){
-		     for(int ic=0; ic<cdim; ic++){
-			for(int ir=0; ir<rdim; ir++){
-	       		   // psi4[l,r,c1,c2] <-> psi3[l,r,c1c2]
-		           int imv = ioff+iv*mdim+im; 
-			   blk3(ir,ic,imv) = blk4(ir,ic,im,iv);
-			} // ir
-		     } // ic
-		  } // im
-	       } // iv
-	    } // p12
-	 } // bc
-      } // br
-   } // bc1c2
+   // loop over qt3
+   int br, bc, bc1c2;
+   for(int i=0; i<qt3.info._nnzaddr.size(); i++){
+      int idx = qt3.info._nnzaddr[i];
+      qt3.info._addr_unpack(idx,br,bc,bc1c2);
+      auto& blk3 = qt3(br,bc,bc1c2);
+      // lover over compatible c1*c2 = c1c2
+      auto qsym_c1c2 = qc1c2.get_sym(bc1c2);
+      for(const auto& p12 : dpt.at(qsym_c1c2)){
+         int bm = std::get<0>(p12);
+         int bv = std::get<1>(p12);
+         int ioff = std::get<2>(p12);
+         const auto& blk4 = qt4(br,bc,bm,bv);
+         if(blk4.size() == 0) continue;
+         int rdim = blk4.dim0;
+         int cdim = blk4.dim1;
+         int mdim = blk4.dim2; 
+         int vdim = blk4.dim3; 
+	 for(int iv=0; iv<vdim; iv++){
+	    for(int im=0; im<mdim; im++){
+	       // psi4[l,r,c1,c2] <-> psi3[l,r,c1c2]
+	       int imv = ioff+iv*mdim+im; 
+	       for(int ic=0; ic<cdim; ic++){
+	   	  for(int ir=0; ir<rdim; ir++){
+	  	     blk3(ir,ic,imv) = blk4(ir,ic,im,iv);
+	          } // ir
+	       } // ic
+	    } // im
+	 } // iv
+      } // p12
+   } // i
    return qt3;
 }
 // psi4[l,r,c1,c2] <- psi3[l,r,c1c2]
@@ -243,38 +238,37 @@ stensor4<Tm> split_qt4_qt3_c1c2(const stensor3<Tm>& qt3,
    const auto& qrow = qt3.info.qrow;
    const auto& qcol = qt3.info.qcol;
    stensor4<Tm> qt4(sym, qrow, qcol, qc1, qc2);
-   for(int bc1c2=0; bc1c2<qc1c2.size(); bc1c2++){
-      for(int br=0; br<qrow.size(); br++){
-	 int rdim = qrow.get_dim(br);
-         for(int bc=0; bc<qcol.size(); bc++){
-	    int cdim = qcol.get_dim(bc);
-	    const auto& blk3 = qt3(br,bc,bc1c2);
-	    if(blk3.size() == 0) continue;
-	    // lover over compatible c1*c2 = c1c2
-	    auto qsym_c1c2 = qc1c2.get_sym(bc1c2);
-	    for(const auto& p12 : dpt.at(qsym_c1c2)){
-	       int bm = std::get<0>(p12);
-	       int bv = std::get<1>(p12);
-	       int ioff = std::get<2>(p12);
-	       int mdim = qt4.info.qmid.get_dim(bm);
-	       int vdim = qt4.info.qver.get_dim(bv);
-	       auto& blk4 = qt4(br,bc,bm,bv);
-	       if(blk4.size() == 0) continue;
-	       for(int iv=0; iv<vdim; iv++){
-		  for(int im=0; im<mdim; im++){
-		     for(int ic=0; ic<cdim; ic++){
-			for(int ir=0; ir<rdim; ir++){
-	       		   // psi4[l,r,c1,c2] <-> psi3[l,r,c1c2]
-		           int imv = ioff+iv*mdim+im; 
-			   blk4(ir,ic,im,iv) = blk3(ir,ic,imv);
-			} // ir
-		     } // ic
-		  } // im
-	       } // iv
-	    } // p12
-	 } // bc
-      } // br
-   } // bc1c2
+   // loop over qt3
+   int br, bc, bc1c2;
+   for(int i=0; i<qt3.info._nnzaddr.size(); i++){
+      int idx = qt3.info._nnzaddr[i];
+      qt3.info._addr_unpack(idx,br,bc,bc1c2);
+      const auto& blk3 = qt3(br,bc,bc1c2);
+      // lover over compatible c1*c2 = c1c2
+      auto qsym_c1c2 = qc1c2.get_sym(bc1c2);
+      for(const auto& p12 : dpt.at(qsym_c1c2)){
+         int bm = std::get<0>(p12);
+         int bv = std::get<1>(p12);
+         int ioff = std::get<2>(p12);
+         auto& blk4 = qt4(br,bc,bm,bv);
+         if(blk4.size() == 0) continue;
+         int rdim = blk4.dim0;
+         int cdim = blk4.dim1;
+         int mdim = blk4.dim2; 
+         int vdim = blk4.dim3; 
+	 for(int iv=0; iv<vdim; iv++){
+	    for(int im=0; im<mdim; im++){
+	       // psi4[l,r,c1,c2] <-> psi3[l,r,c1c2]
+	       int imv = ioff+iv*mdim+im; 
+	       for(int ic=0; ic<cdim; ic++){
+	  	  for(int ir=0; ir<rdim; ir++){
+	  	     blk4(ir,ic,im,iv) = blk3(ir,ic,imv);
+	  	  } // ir
+	       } // ic
+	    } // im
+	 } // iv
+      } // p12
+   } // i
    return qt4;
 }
 
