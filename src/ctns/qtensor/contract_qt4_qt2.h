c8222ca337a6d0b267d6005a0fa7f8f90149611f
zhendongli2008@gmail.com <zhendongli2008@gmail.com>
2022-02-23 20:13:46 +0800

    update examples and qt4_qt2

--- src/ctns/qtensor/contract_qt4_qt2.h
+++ src/ctns/qtensor/contract_qt4_qt2.h
@@ -33,6 +33,7 @@ template <typename Tm>
 stensor4<Tm> contract_qt4_qt2_l(const stensor4<Tm>& qt4a, 
 				const stensor2<Tm>& qt2,
 				const bool ifdagger=false){
+/*
    const auto& qt2b = ifdagger? qt2.H() : qt2;
    assert(qt4a.dir_row() == !qt2b.dir_col());
    assert(qt4a.info.qrow == qt2b.info.qcol);
@@ -58,6 +59,38 @@ stensor4<Tm> contract_qt4_qt2_l(const stensor4<Tm>& qt4a,
             } // im
          } // iv
       } // bx
+   } // i
+*/
+   auto sym2 = ifdagger? -qt2.info.sym : qt2.info.sym;
+   auto qext = ifdagger? qt2.info.qcol : qt2.info.qrow; 
+   auto qint = ifdagger? qt2.info.qrow : qt2.info.qcol;
+   auto dext = qt2.dir_row(); // see the comment in stensor2<Tm>::H()
+   auto dint = qt2.dir_col();
+   assert(qt4a.dir_row() == !dint);
+   assert(qt4a.info.qrow == qint);
+   qsym sym = qt4a.info.sym + sym2;
+   stensor4<Tm> qt4(sym, qext, qt4a.info.qcol, qt4a.info.qmid, qt4a.info.qver);
+   // loop over external indices
+   const char* transa = ifdagger? "C" : "N";
+   const Tm alpha = 1.0, beta = 1.0;
+   int br, bc, bm, bv;
+   for(int i=0; i<qt4.info._nnzaddr.size(); i++){
+      int idx = qt4.info._nnzaddr[i];
+      qt4.info._addr_unpack(idx,br,bc,bm,bv);
+      auto& blk4 = qt4(br,bc,bm,bv);
+      // loop over contracted indices
+      for(int bx=0; bx<qt4a.rows(); bx++){
+         const auto& blk4a = qt4a(bx,bc,bm,bv);
+         const auto& blk2b = ifdagger? qt2(bx,br) : qt2(br,bx);
+         if(blk4a.size() == 0 || blk2b.size() == 0) continue;
+         int mdim = blk4.dim2;
+         int vdim = blk4.dim3;
+         for(int iv=0; iv<vdim; iv++){
+            for(int im=0; im<mdim; im++){
+               xgemm(transa,"N",1.0,blk2b,blk4a.get(im,iv),1.0,blk4.get(im,iv));
+            } // im
+         } // iv
+      } // bx
    } // i 
    return qt4;
 }
@@ -69,6 +102,7 @@ template <typename Tm>
 stensor4<Tm> contract_qt4_qt2_r(const stensor4<Tm>& qt4a, 
 				const stensor2<Tm>& qt2,
 				const bool ifdagger=false){
+/*
    const auto& qt2b = ifdagger? qt2.H() : qt2;
    assert(qt4a.dir_col() == !qt2b.dir_col()); // each line is associated with one dir
    assert(qt4a.info.qcol == qt2b.info.qcol);
@@ -96,6 +130,41 @@ stensor4<Tm> contract_qt4_qt2_r(const stensor4<Tm>& qt4a,
          } // iv
       } // bx
    } // i
+*/
+   auto sym2 = ifdagger? -qt2.info.sym : qt2.info.sym;
+   auto qext = ifdagger? qt2.info.qcol : qt2.info.qrow; 
+   auto qint = ifdagger? qt2.info.qrow : qt2.info.qcol;
+   auto dext = qt2.dir_row(); // see the comment in stensor2<Tm>::H()
+   auto dint = qt2.dir_col();
+   assert(qt4a.dir_col() == !dint);
+   assert(qt4a.info.qcol == qint);
+   qsym sym = qt4a.info.sym + sym2;
+   stensor4<Tm> qt4(sym, qt4a.info.qrow, qext, qt4a.info.qmid, qt4a.info.qver);
+   // loop over external indices
+   const char* transb = ifdagger? "N" : "T";
+   const Tm alpha = 1.0, beta = 1.0;
+   int br, bc, bm, bv;
+   for(int i=0; i<qt4.info._nnzaddr.size(); i++){
+      int idx = qt4.info._nnzaddr[i];
+      qt4.info._addr_unpack(idx,br,bc,bm,bv);
+      auto& blk4 = qt4(br,bc,bm,bv);
+      // loop over contracted indices
+      for(int bx=0; bx<qt4a.cols(); bx++){
+         const auto& blk4a = qt4a(br,bx,bm,bv);
+         auto blk2b = ifdagger? qt2(bx,bc) : qt2(bc,bx);
+         if(blk4a.size() == 0 || blk2b.size() == 0) continue;
+         // sigma(r,c,m,v) = op(c,x)*wf(r,x,m,v)
+         int mdim = blk4.dim2;
+         int vdim = blk4.dim3;
+         if(ifdagger) blk2b.conjugate();
+         for(int iv=0; iv<vdim; iv++){
+            for(int im=0; im<mdim; im++){
+               xgemm("N",transb,alpha,blk4a.get(im,iv),blk2b,beta,blk4.get(im,iv));
+            } // im
+         } // iv
+	 if(ifdagger) blk2b.conjugate();
+      } // bx
+   } // i
    return qt4;
 }
 
@@ -108,6 +177,7 @@ template <typename Tm>
 stensor4<Tm> contract_qt4_qt2_c1(const stensor4<Tm>& qt4a, 
 			 	 const stensor2<Tm>& qt2,
 				 const bool ifdagger=false){
+/*
    const auto& qt2b = ifdagger? qt2.H() : qt2;
    assert(qt4a.dir_mid() == !qt2b.dir_col());
    assert(qt4a.info.qmid == qt2b.info.qcol);
@@ -140,6 +210,44 @@ stensor4<Tm> contract_qt4_qt2_c1(const stensor4<Tm>& qt4a,
          } // iv
       } // bx
    } // i
+*/
+   auto sym2 = ifdagger? -qt2.info.sym : qt2.info.sym;
+   auto qext = ifdagger? qt2.info.qcol : qt2.info.qrow; 
+   auto qint = ifdagger? qt2.info.qrow : qt2.info.qcol;
+   auto dext = qt2.dir_row(); // see the comment in stensor2<Tm>::H()
+   auto dint = qt2.dir_col();
+   assert(qt4a.dir_mid() == !dint);
+   assert(qt4a.info.qmid == qint);
+   qsym sym = qt4a.info.sym + sym2;
+   stensor4<Tm> qt4(sym, qt4a.info.qrow, qt4a.info.qcol, qext, qt4a.info.qver);
+   // loop over external indices
+   const char* transb = ifdagger? "N" : "T";
+   const Tm alpha = 1.0, beta = 1.0;
+   int br, bc, bm, bv;
+   for(int i=0; i<qt4.info._nnzaddr.size(); i++){
+      int idx = qt4.info._nnzaddr[i];
+      qt4.info._addr_unpack(idx,br,bc,bm,bv);
+      auto& blk4 = qt4(br,bc,bm,bv);
+      // loop over contracted indices
+      for(int bx=0; bx<qt4a.mids(); bx++){
+         const auto& blk4a = qt4a(br,bc,bx,bv);
+         auto blk2b = ifdagger? qt2(bx,bm) : qt2(bm,bx);
+         if(blk4a.size() == 0 || blk2b.size() == 0) continue;
+         // sigma(rc,m,v) = op(m,x)*wf(rc,x,v)
+         int rcdim = blk4.dim0*blk4.dim1;
+         int mdim = blk4.dim2;
+         int xdim = blk4a.dim2;
+         int vdim = blk4.dim3;
+	 if(ifdagger) blk2b.conjugate();
+         int LDB = ifdagger? xdim : mdim;
+         for(int iv=0; iv<vdim; iv++){
+            linalg::xgemm("N", transb, &rcdim, &mdim, &xdim, &alpha,
+                          blk4a.get(iv).data(), &rcdim, blk2b.data(), &LDB, &beta,
+ 	                  blk4.get(iv).data(), &rcdim);
+         } // iv
+	 if(ifdagger) blk2b.conjugate();
+      } // bx
+   } // i
    return qt4;
 }
 
@@ -152,6 +260,7 @@ template <typename Tm>
 stensor4<Tm> contract_qt4_qt2_c2(const stensor4<Tm>& qt4a, 
 			 	 const stensor2<Tm>& qt2,
 				 const bool ifdagger=false){
+/*
    const auto& qt2b = ifdagger? qt2.H() : qt2;
    assert(qt4a.dir_ver() == !qt2b.dir_col());
    assert(qt4a.info.qver == qt2b.info.qcol);
@@ -179,6 +288,41 @@ stensor4<Tm> contract_qt4_qt2_c2(const stensor4<Tm>& qt4a,
 			     blk4.get(iv).data());
             } // ix
          } // iv
+      } // bx
+   } // i
+*/
+   auto sym2 = ifdagger? -qt2.info.sym : qt2.info.sym;
+   auto qext = ifdagger? qt2.info.qcol : qt2.info.qrow; 
+   auto qint = ifdagger? qt2.info.qrow : qt2.info.qcol;
+   auto dext = qt2.dir_row(); // see the comment in stensor2<Tm>::H()
+   auto dint = qt2.dir_col();
+   assert(qt4a.dir_ver() == !dint);
+   assert(qt4a.info.qver == qint);
+   qsym sym = qt4a.info.sym + sym2;
+   stensor4<Tm> qt4(sym, qt4a.info.qrow, qt4a.info.qcol, qt4a.info.qmid, qext);
+   // loop over external indices
+   const char* transb = ifdagger? "N" : "T";
+   const Tm alpha = 1.0, beta = 1.0;
+   int br, bc, bm, bv;
+   for(int i=0; i<qt4.info._nnzaddr.size(); i++){
+      int idx = qt4.info._nnzaddr[i];
+      qt4.info._addr_unpack(idx,br,bc,bm,bv);
+      auto& blk4 = qt4(br,bc,bm,bv);
+      // loop over contracted indices
+      for(int bx=0; bx<qt4a.vers(); bx++){
+         const auto& blk4a = qt4a(br,bc,bm,bx);
+         auto blk2b = ifdagger? qt2(bx,bv) : qt2(bv,bx);
+         if(blk4a.size() == 0 || blk2b.size() == 0) continue;
+         // sigma(rcm,v) = op(v,x)*wf(rcm,x)
+         int rcmdim = blk4.dim0*blk4.dim1*blk4.dim2;
+         int xdim = blk4a.dim3;
+         int vdim = blk4.dim3;
+	 if(ifdagger) blk2b.conjugate();
+         int LDB = ifdagger? xdim : vdim;
+	 linalg::xgemm("N", transb, &rcmdim, &vdim, &xdim, &alpha,
+		       blk4a.data(), &rcmdim, blk2b.data(), &LDB, &beta,
+		       blk4.data(), &rcmdim);
+         if(ifdagger) blk2b.conjugate();
       } // bx
    } // i
    return qt4;
