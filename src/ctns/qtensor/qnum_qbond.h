2c2ded92e0f5fa8a80ede310836b0cb7ad725ba6
zhendongli <zhendongli2008@gmail.com>
2022-09-02 10:59:16 +0800

    sort_by_dim in decimation and qmerge

--- src/ctns/qtensor/qnum_qbond.h
+++ src/ctns/qtensor/qnum_qbond.h
@@ -3,71 +3,86 @@
 
 namespace ctns{
 
-const int maxdim_per_sym = 65536; // 2**16
-extern const int maxdim_per_sym;
+   const int maxdim_per_sym = 65536; // 2**16
+   extern const int maxdim_per_sym; // used in sweep_twodot_diag
 
-// qbond: std::vector<std::pair<qsym,int>> dims;
-class qbond{
-   private:
-      friend class boost::serialization::access;	   
-      template<class Archive>
-      void serialize(Archive & ar, const unsigned int version){
-	 ar & dims;
-      }
-   public:
-      // constructor
-      qbond(){};
-      qbond(const std::vector<std::pair<qsym,int>>& ds): dims(ds) {}
-      // helpers
-      int size() const{ return dims.size(); }
-      qsym get_sym(const int i) const{ return dims[i].first; } 
-      int get_dim(const int i) const{ return dims[i].second; }
-      int get_parity(const int i) const{ return dims[i].first.parity(); }
-      // total dimension
-      int get_dimAll() const{
-         int dim = 0;
-         for(const auto& p : dims) dim += p.second;
-         return dim;
-      }
-      // offset 
-      std::vector<int> get_offset() const{
-         std::vector<int> offset;
-         int ioff = 0;
-	 for(int i=0; i<dims.size(); i++){
-	    offset.push_back(ioff);
-	    ioff += dims[i].second; 
-	 }
-	 return offset;
-      }
-      // comparison
-      bool operator ==(const qbond& qs) const{
-	 bool ifeq = (dims.size() == qs.size());
-	 if(not ifeq) return false;
-	 for(int i=0; i<dims.size(); i++){
-	    ifeq = ifeq && dims[i].first == qs.dims[i].first &&
-		           dims[i].second == qs.dims[i].second;
-	    if(not ifeq) return false;
-	 }
-	 return true;
-      }
-      void print(const std::string name, const bool debug=true) const{
-	 std::cout << " qbond: " << name 
-		   << " nsym=" << dims.size()
-      	           << " dimAll=" << get_dimAll() 
-		   << std::endl;
-         // loop over symmetry sectors
-	 if(debug){
-            for(int i=0; i<dims.size(); i++){
-               auto sym = dims[i].first;
-               auto dim = dims[i].second;
-	       std::cout << " " << sym << ":" << dim;
+   // qbond: std::vector<std::pair<qsym,int>> dims;
+   class qbond{
+      private:
+         friend class boost::serialization::access;	   
+         template<class Archive>
+            void serialize(Archive & ar, const unsigned int version){
+               ar & dims;
             }
-	    std::cout << std::endl;
-	 }
-      }
-   public:
-      std::vector<std::pair<qsym,int>> dims;
-};
+      public:
+         // constructor
+         qbond(){};
+         qbond(const std::vector<std::pair<qsym,int>>& ds): dims(ds) {}
+         // order
+         void sort_by_sym(){
+            std::stable_sort(dims.begin(), dims.end(),
+                  [](const std::pair<qsym,int>& t1,
+                     const std::pair<qsym,int>& t2){
+                  return t2.first < t1.first;
+                  });
+         }
+         void sort_by_dim(){
+            std::stable_sort(dims.begin(), dims.end(),
+                  [](const std::pair<qsym,int>& t1,
+                     const std::pair<qsym,int>& t2){
+                  return t2.second < t1.second;
+                  });
+         }
+         // helpers
+         int size() const{ return dims.size(); }
+         qsym get_sym(const int i) const{ return dims[i].first; } 
+         int get_dim(const int i) const{ return dims[i].second; }
+         int get_parity(const int i) const{ return dims[i].first.parity(); }
+         // total dimension
+         int get_dimAll() const{
+            int dim = 0;
+            for(const auto& p : dims) dim += p.second;
+            return dim;
+         }
+         // offset 
+         std::vector<int> get_offset() const{
+            std::vector<int> offset;
+            int ioff = 0;
+            for(int i=0; i<dims.size(); i++){
+               offset.push_back(ioff);
+               ioff += dims[i].second; 
+            }
+            return offset;
+         }
+         // comparison
+         bool operator ==(const qbond& qs) const{
+            bool ifeq = (dims.size() == qs.size());
+            if(not ifeq) return false;
+            for(int i=0; i<dims.size(); i++){
+               ifeq = ifeq && dims[i].first == qs.dims[i].first &&
+                  dims[i].second == qs.dims[i].second;
+               if(not ifeq) return false;
+            }
+            return true;
+         }
+         void print(const std::string name, const bool debug=true) const{
+            std::cout << " qbond: " << name 
+               << " nsym=" << dims.size()
+               << " dimAll=" << get_dimAll() 
+               << std::endl;
+            // loop over symmetry sectors
+            if(debug){
+               for(int i=0; i<dims.size(); i++){
+                  auto sym = dims[i].first;
+                  auto dim = dims[i].second;
+                  std::cout << " " << sym << ":" << dim;
+               }
+               std::cout << std::endl;
+            }
+         }
+      public:
+         std::vector<std::pair<qsym,int>> dims;
+   };
 
 } // ctns
 
--- src/ctns/qtensor/qnum_qdpt.h
+++ src/ctns/qtensor/qnum_qdpt.h
@@ -6,164 +6,169 @@
 
 namespace ctns{
 
-// direct product table of qbond : V1*V2->V12
-// qsym -> list of {(idx1,idx2,ioff)}
-using qdpt = std::map<qsym,std::vector<std::tuple<int,int,int>>>;
-using qproduct = std::pair<qbond,qdpt>;
+   // direct product table of qbond : V1*V2->V12
+   // qsym -> list of {(idx1,idx2,ioff)}
+   using qdpt = std::map<qsym,std::vector<std::tuple<int,int,int>>>;
+   using qproduct = std::pair<qbond,qdpt>;
 
-// qs12 = qs1*qs2
-inline qproduct qmerge(const qbond& qs1, const qbond& qs2){
-   // init dpt
-   qdpt dpt;
-   for(int i1=0; i1<qs1.size(); i1++){
-      auto q1 = qs1.get_sym(i1);
-      for(int i2=0; i2<qs2.size(); i2++){
-	 auto q2 = qs2.get_sym(i2);
-	 dpt[q1+q2].push_back(std::make_tuple(i1,i2,0));
-      }
-   }
-   // form qs12 & compute offset
-   qbond qs12;
-   for(auto& p : dpt){
-      const auto& q12 = p.first;
-      auto& p12 = p.second;
-      int ioff = 0;
-      for(int i12=0; i12<p12.size(); i12++){
-         int i1 = std::get<0>(p12[i12]);
-         int i2 = std::get<1>(p12[i12]);
-         int d1 = qs1.get_dim(i1);
-	 int d2 = qs2.get_dim(i2);
-         p12[i12] = std::make_tuple(i1,i2,ioff);
-	 ioff += d1*d2; 
-      }
-      qs12.dims.push_back(std::make_pair(q12,ioff));
-   }
-   return std::make_pair(qs12,dpt);
-}
+   const bool sort_by_dim = true;
+   extern const bool sort_by_dim; 
 
-// mapping from original PRODUCT basis to kramers paired basis:
-// V[odd] = {|le,ro>,|lo,re>}
-inline void mapping2krbasis_odd(const qsym& qr,
-				const qbond& qs1,
-		                const qbond& qs2,
-		                const qdpt& dpt,
-		                std::vector<int>& pos_new,
-				std::vector<double>& phases){
-   std::vector<int> pos_up, pos_dw;
-   const auto& combination = dpt.at(qr);
-   for(int i=0; i<combination.size(); i++){
-      int b1   = std::get<0>(combination[i]);
-      int b2   = std::get<1>(combination[i]);
-      int ioff = std::get<2>(combination[i]);
-      auto q1 = qs1.get_sym(b1);
-      auto q2 = qs2.get_sym(b2);
-      int  d1 = qs1.get_dim(b1);
-      int  d2 = qs2.get_dim(b2);
-      // |le,ro> 
-      if(q1.parity() == 0 && q2.parity() == 1){
-         assert(d2%2 == 0);
-         for(int i2=0; i2<d2/2; i2++){
-            for(int i1=0; i1<d1; i1++){
-               int idxA = ioff + i2*d1 + i1; // |le,ro>
-               pos_up.push_back(idxA);
-               int idxB = ioff + (i2+d2/2)*d1 + i1; // |le,ro_bar>
-               pos_dw.push_back(idxB);
-            }
+   // qs12 = qs1*qs2
+   inline qproduct qmerge(const qbond& qs1, const qbond& qs2){
+      // init dpt
+      qdpt dpt;
+      for(int i1=0; i1<qs1.size(); i1++){
+         auto q1 = qs1.get_sym(i1);
+         for(int i2=0; i2<qs2.size(); i2++){
+            auto q2 = qs2.get_sym(i2);
+            dpt[q1+q2].push_back(std::make_tuple(i1,i2,0));
          }
-      // |lo,re>   
-      }else if(q1.parity() == 1 && q2.parity() == 0){
-         assert(d1%2 == 0);
-         for(int i2=0; i2<d2; i2++){
-            for(int i1=0; i1<d1/2; i1++){
-   	       int idxA = ioff + i2*d1 + i1; 
-               pos_up.push_back(idxA);
-     	       int idxB = ioff + i2*d1 + (i1+d1/2);
-               pos_dw.push_back(idxB);
-            }
+      }
+      // form qs12 & compute offset
+      qbond qs12;
+      for(auto& p : dpt){
+         const auto& q12 = p.first;
+         auto& p12 = p.second;
+         int ioff = 0;
+         for(int i12=0; i12<p12.size(); i12++){
+            int i1 = std::get<0>(p12[i12]);
+            int i2 = std::get<1>(p12[i12]);
+            int d1 = qs1.get_dim(i1);
+            int d2 = qs2.get_dim(i2);
+            p12[i12] = std::make_tuple(i1,i2,ioff);
+            ioff += d1*d2; 
          }
+         qs12.dims.push_back(std::make_pair(q12,ioff));
+      }
+      // reoder qs12
+      if(sort_by_dim) qs12.sort_by_dim();
+      return std::make_pair(qs12,dpt);
+   }
+
+   // mapping from original PRODUCT basis to kramers paired basis:
+   // V[odd] = {|le,ro>,|lo,re>}
+   inline void mapping2krbasis_odd(const qsym& qr,
+         const qbond& qs1,
+         const qbond& qs2,
+         const qdpt& dpt,
+         std::vector<int>& pos_new,
+         std::vector<double>& phases){
+      std::vector<int> pos_up, pos_dw;
+      const auto& combination = dpt.at(qr);
+      for(int i=0; i<combination.size(); i++){
+         int b1   = std::get<0>(combination[i]);
+         int b2   = std::get<1>(combination[i]);
+         int ioff = std::get<2>(combination[i]);
+         auto q1 = qs1.get_sym(b1);
+         auto q2 = qs2.get_sym(b2);
+         int  d1 = qs1.get_dim(b1);
+         int  d2 = qs2.get_dim(b2);
+         // |le,ro> 
+         if(q1.parity() == 0 && q2.parity() == 1){
+            assert(d2%2 == 0);
+            for(int i2=0; i2<d2/2; i2++){
+               for(int i1=0; i1<d1; i1++){
+                  int idxA = ioff + i2*d1 + i1; // |le,ro>
+                  pos_up.push_back(idxA);
+                  int idxB = ioff + (i2+d2/2)*d1 + i1; // |le,ro_bar>
+                  pos_dw.push_back(idxB);
+               }
+            }
+            // |lo,re>   
+         }else if(q1.parity() == 1 && q2.parity() == 0){
+            assert(d1%2 == 0);
+            for(int i2=0; i2<d2; i2++){
+               for(int i1=0; i1<d1/2; i1++){
+                  int idxA = ioff + i2*d1 + i1; 
+                  pos_up.push_back(idxA);
+                  int idxB = ioff + i2*d1 + (i1+d1/2);
+                  pos_dw.push_back(idxB);
+               }
+            }
+         }else{
+            std::cout << "q1p,q2p=" << q1.parity() << "," << q2.parity() << std::endl;
+            tools::exit("error: no such combination of parities!");
+         }
+         ioff += d1*d2;
+      }
+      assert(pos_up.size() == pos_dw.size());
+      pos_new.clear();
+      pos_new.insert(pos_new.end(), pos_up.begin(), pos_up.end());
+      pos_new.insert(pos_new.end(), pos_dw.begin(), pos_dw.end());
+      phases.resize(pos_dw.size(),1.0);
+   }
+
+   // V[even] = {|le,re>,|lo,ro>}
+   inline void mapping2krbasis_even(const qsym& qr,
+         const qbond& qs1,
+         const qbond& qs2,
+         const qdpt& dpt,
+         std::vector<int>& pos_new,
+         std::vector<double>& phases){
+      std::vector<int> pos_up, pos_dw, pos_ee;
+      const auto& combination = dpt.at(qr);
+      for(int i=0; i<combination.size(); i++){
+         int b1   = std::get<0>(combination[i]);
+         int b2   = std::get<1>(combination[i]);
+         int ioff = std::get<2>(combination[i]);
+         auto q1 = qs1.get_sym(b1);
+         auto q2 = qs2.get_sym(b2);
+         int  d1 = qs1.get_dim(b1);
+         int  d2 = qs2.get_dim(b2);
+         // |le,re> 
+         if(q1.parity() == 0 && q2.parity() == 0){
+            for(int i2=0; i2<d2; i2++){
+               for(int i1=0; i1<d1; i1++){
+                  int idx = ioff + i2*d1 + i1;
+                  pos_ee.push_back(idx);
+               }
+            }
+            // |lo,ro> = {|lo,ro>,|lo_bar,ro>} + {|lo_bar,ro_bar>,|lo,ro_bar>}
+         }else if(q1.parity() == 1 && q2.parity() == 1){
+            assert(d1%2 == 0 && d2%2 == 0);
+            for(int i2=0; i2<d2/2; i2++){
+               for(int i1=0; i1<d1/2; i1++){
+                  int idxA = ioff + i2*d1 + i1; // |lo,ro> 
+                  pos_up.push_back(idxA);
+                  int idxB = ioff + (i2+d2/2)*d1 + (i1+d1/2); // |lo_bar,ro_bar>
+                  pos_dw.push_back(idxB);
+                  phases.push_back(1.0);
+               }
+               for(int i1=0; i1<d1/2; i1++){
+                  int idxA = ioff + i2*d1 + (i1+d1/2); // |lo_bar,ro> 
+                  pos_up.push_back(idxA);
+                  int idxB = ioff + (i2+d2/2)*d1 + i1; // |lo,ro_bar>
+                  pos_dw.push_back(idxB);
+                  phases.push_back(-1.0);
+               }
+            }
+         }else{
+            std::cout << "q1p,q2p=" << q1.parity() << "," << q2.parity() << std::endl;
+            tools::exit("error: no such combination of parities!");
+         }
+         ioff += d1*d2;
+      }
+      assert(pos_up.size() == pos_dw.size());
+      pos_new.clear();
+      pos_new.insert(pos_new.end(), pos_up.begin(), pos_up.end());
+      pos_new.insert(pos_new.end(), pos_dw.begin(), pos_dw.end());
+      pos_new.insert(pos_new.end(), pos_ee.begin(), pos_ee.end());
+   }
+
+   // mapping product basis to kramers paired basis 
+   inline void mapping2krbasis(const qsym& qr,
+         const qbond& qs1,
+         const qbond& qs2,
+         const qdpt& dpt,
+         std::vector<int>& pos_new,
+         std::vector<double>& phases){
+      if(qr.parity() == 1){
+         mapping2krbasis_odd(qr,qs1,qs2,dpt,pos_new,phases);
       }else{
-         std::cout << "q1p,q2p=" << q1.parity() << "," << q2.parity() << std::endl;
-	 tools::exit("error: no such combination of parities!");
+         mapping2krbasis_even(qr,qs1,qs2,dpt,pos_new,phases);
       }
-      ioff += d1*d2;
    }
-   assert(pos_up.size() == pos_dw.size());
-   pos_new.clear();
-   pos_new.insert(pos_new.end(), pos_up.begin(), pos_up.end());
-   pos_new.insert(pos_new.end(), pos_dw.begin(), pos_dw.end());
-   phases.resize(pos_dw.size(),1.0);
-}
-
-// V[even] = {|le,re>,|lo,ro>}
-inline void mapping2krbasis_even(const qsym& qr,
-			         const qbond& qs1,
-			         const qbond& qs2,
-			         const qdpt& dpt,
-		                 std::vector<int>& pos_new,
-				 std::vector<double>& phases){
-   std::vector<int> pos_up, pos_dw, pos_ee;
-   const auto& combination = dpt.at(qr);
-   for(int i=0; i<combination.size(); i++){
-      int b1   = std::get<0>(combination[i]);
-      int b2   = std::get<1>(combination[i]);
-      int ioff = std::get<2>(combination[i]);
-      auto q1 = qs1.get_sym(b1);
-      auto q2 = qs2.get_sym(b2);
-      int  d1 = qs1.get_dim(b1);
-      int  d2 = qs2.get_dim(b2);
-      // |le,re> 
-      if(q1.parity() == 0 && q2.parity() == 0){
-         for(int i2=0; i2<d2; i2++){
-            for(int i1=0; i1<d1; i1++){
-               int idx = ioff + i2*d1 + i1;
-               pos_ee.push_back(idx);
-            }
-         }
-      // |lo,ro> = {|lo,ro>,|lo_bar,ro>} + {|lo_bar,ro_bar>,|lo,ro_bar>}
-      }else if(q1.parity() == 1 && q2.parity() == 1){
-         assert(d1%2 == 0 && d2%2 == 0);
-         for(int i2=0; i2<d2/2; i2++){
-            for(int i1=0; i1<d1/2; i1++){
-               int idxA = ioff + i2*d1 + i1; // |lo,ro> 
-               pos_up.push_back(idxA);
-               int idxB = ioff + (i2+d2/2)*d1 + (i1+d1/2); // |lo_bar,ro_bar>
-     	       pos_dw.push_back(idxB);
-               phases.push_back(1.0);
-	    }
-            for(int i1=0; i1<d1/2; i1++){
-               int idxA = ioff + i2*d1 + (i1+d1/2); // |lo_bar,ro> 
-               pos_up.push_back(idxA);
-               int idxB = ioff + (i2+d2/2)*d1 + i1; // |lo,ro_bar>
-     	       pos_dw.push_back(idxB);
-               phases.push_back(-1.0);
-            }
-         }
-      }else{
-         std::cout << "q1p,q2p=" << q1.parity() << "," << q2.parity() << std::endl;
-	 tools::exit("error: no such combination of parities!");
-      }
-      ioff += d1*d2;
-   }
-   assert(pos_up.size() == pos_dw.size());
-   pos_new.clear();
-   pos_new.insert(pos_new.end(), pos_up.begin(), pos_up.end());
-   pos_new.insert(pos_new.end(), pos_dw.begin(), pos_dw.end());
-   pos_new.insert(pos_new.end(), pos_ee.begin(), pos_ee.end());
-}
-
-// mapping product basis to kramers paired basis 
-inline void mapping2krbasis(const qsym& qr,
-		            const qbond& qs1,
-		            const qbond& qs2,
-		            const qdpt& dpt,
-		            std::vector<int>& pos_new,
-		            std::vector<double>& phases){
-   if(qr.parity() == 1){
-      mapping2krbasis_odd(qr,qs1,qs2,dpt,pos_new,phases);
-   }else{
-      mapping2krbasis_even(qr,qs1,qs2,dpt,pos_new,phases);
-   }
-}
 
 } // ctns
 
