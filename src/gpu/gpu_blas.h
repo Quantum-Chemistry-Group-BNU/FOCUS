f60eeca8106453faa34beec563c1366efc3e8473
xiangchunyang <zzucyxiang@gmail.com>
2023-03-01 20:07:07 +0800

    transform batch daxpy to dgemv

--- src/gpu/gpu_blas.h
+++ src/gpu/gpu_blas.h
@@ -30,28 +30,45 @@ namespace linalg{
          const double* X, const int* INCX,
          const double* beta, double* Y, const int* INCY ){
       magma_trans_t transA =  MagmaNoTrans ;
-      if(*TRANSA=='T'){
+      if(TRANSA[0]=='T'){
          transA = MagmaTrans;
-      }else if (*TRANSA == 'C'){
+      }else if (TRANSA[0] == 'C'){
          transA = MagmaConjTrans;
       }
+      double* dev_X = nullptr;
+      dev_X = (double*)gpumem.allocate((*N)*sizeof(double));
+#ifdef USE_HIP
+            HIP_CHECK(hipMemcpy(dev_X, X, (*N)*sizeof(double), hipMemcpyHostToDevice));
+#else
+            CUDA_CHECK(cudaMemcpy(dev_X, X, (*N)*sizeof(double), cudaMemcpyHostToDevice));
+#endif// USE_HIP
       magma_dgemv(transA, *M, *N, *alpha, A, *LDA,
-            X, *INCX, *beta, Y, *INCY, magma_queue);
+            dev_X, *INCX, *beta, Y, *INCY, magma_queue);
+
+      gpumem.deallocate(dev_X, (*N)*sizeof(double));
    }
    inline void xgemv_magma(const char* TRANSA, const int* M, const int* N,
          const std::complex<double>* alpha, const std::complex<double>* A, const int* LDA,
          const std::complex<double>* X, const int* INCX,
          const std::complex<double>* beta, std::complex<double>* Y, const int* INCY){
       magma_trans_t transA =  MagmaNoTrans ;
-      if(*TRANSA == 'T'){
+      if(TRANSA[0] == 'T'){
          transA = MagmaTrans;
-      }else if (*TRANSA == 'C'){
+      }else if (TRANSA[0] == 'C'){
          transA = MagmaConjTrans;
       }
+      magmaDoubleComplex* dev_X = nullptr;
+      dev_X = (magmaDoubleComplex*)gpumem.allocate((*N)*sizeof(magmaDoubleComplex));
+#ifdef USE_HIP
+            HIP_CHECK(hipMemcpy(dev_X, X, (*N)*sizeof(magmaDoubleComplex), hipMemcpyHostToDevice));
+#else
+            CUDA_CHECK(cudaMemcpy(dev_X, X, (*N)*sizeof(magmaDoubleComplex), cudaMemcpyHostToDevice));
+#endif// USE_HIP
       magmaDoubleComplex alpha1{alpha->real(),alpha->imag()};
       magmaDoubleComplex beta1{beta->real(),beta->imag()};
       magma_zgemv(transA, *M, *N, alpha1, (magmaDoubleComplex *)A, *LDA,
-            (magmaDoubleComplex *)X, *INCX, beta1, (magmaDoubleComplex *)Y, *INCY, magma_queue);
+            (magmaDoubleComplex *)dev_X, *INCX, beta1, (magmaDoubleComplex *)Y, *INCY, magma_queue);
+            gpumem.deallocate(dev_X, (*N)*sizeof(magmaDoubleComplex));
    }
 } // linalg
 
