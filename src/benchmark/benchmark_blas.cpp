d01c3c0c71f0230ac49067626efe6e63f20776b6
zhendongli <zhendongli2008@gmail.com>
2023-06-11 18:18:31 +0800

    benchmark

--- src/benchmark/benchmark_blas.cpp
+++ src/benchmark/benchmark_blas.cpp
@@ -1,42 +1,52 @@
+#include <memory>
 #include "../core/matrix.h"
-#include "../core/linalg.h"
+#include "../core/blas.h"
+#include "../core/blas_batch.h"
 #include "../core/tools.h"
 
+using namespace std;
+
 template <typename Tm>
-void test_xgemv_batch(const int M, const int N, const int nbatch){
-   char* transa_array = new char[nbatch];
-   transa_array[0] = 'N';
-   MKL_INT* m_array = new MKL_INT[nbatch];
-   MKL_INT* n_array = new MKL_INT[nbatch];
-   Tm *alpha_array = new Tm[nbatch];
-   Tm *beta_array = new Tm[nbatch];
-   alpha_array[0] = 1.0;
-   beta_array[0] = 0.0;
-   MKL_INT *incx_array = new MKL_INT[nbatch];
-   MKL_INT *incy_array = new MKL_INT[nbatch];
-   incx_array[0] = 1;
-   incy_array[0] = 1;
+double test_xgemv_batch(const int M, const int N, const int nbatch){
+   unique_ptr<char[]> transa_array(new char[nbatch]);
+   unique_ptr<MKL_INT[]> m_array(new MKL_INT[nbatch]);
+   unique_ptr<MKL_INT[]> n_array(new MKL_INT[nbatch]);
+   unique_ptr<Tm[]> alpha_array(new Tm[nbatch]);
+   unique_ptr<Tm[]>  beta_array(new Tm[nbatch]);
+   unique_ptr<MKL_INT[]> incx_array(new MKL_INT[nbatch]);
+   unique_ptr<MKL_INT[]> incy_array(new MKL_INT[nbatch]);
+   unique_ptr<MKL_INT[]> group_size(new MKL_INT[nbatch]);
    MKL_INT group_count = nbatch;
-   MKL_INT* group_size = new MKL_INT[nbatch];
    for(int i=0; i<nbatch; i++){
       m_array[i] = M;
       n_array[i] = N;
+      transa_array[i] = 'N';
+      alpha_array[i] = 1.0;
+      beta_array[i] = 0.0;
+      incx_array[i] = 1;
+      incy_array[i] = 1;
       group_size[i] = 1;
    }
    auto a = linalg::random_matrix<Tm>(M*N,nbatch);
    auto x = linalg::random_matrix<Tm>(N*1,nbatch);
    auto y = linalg::random_matrix<Tm>(M*1,nbatch);
-   Tm** A_array = new Tm*[nbatch];
-   Tm** X_array = new Tm*[nbatch];
-   Tm** Y_array = new Tm*[nbatch];
+   std::vector<const Tm*> A_array(nbatch);
+   std::vector<const Tm*> X_array(nbatch);
+   std::vector<Tm*> Y_array(nbatch);
    for(int i=0; i<nbatch; i++){
       A_array[i] = a.col(i);
       X_array[i] = x.col(i);
       Y_array[i] = y.col(i);
    }
-   linalg::xgemv_batch(transa_array, m_array, n_array, alpha_array, A_array, m_array,
-         X_array, incx_array, beta_array, Y_array, incy_array,
-         &group_count, group_size);
+   auto t0 = tools::get_time();
+   linalg::xgemv_batch(transa_array.get(), m_array.get(), n_array.get(), 
+         alpha_array.get(), A_array.data(), m_array.get(),
+         X_array.data(), incx_array.get(), beta_array.get(), 
+         Y_array.data(), incy_array.get(),
+         &group_count, group_size.get());
+   auto t1 = tools::get_time();
+   double dt = tools::get_duration(t1-t0);
+   return dt;
 }
 
 int main(){
@@ -46,14 +56,11 @@ int main(){
 
    using Tm = double;
 
-   std::cout << "GEMV_BATCH" << std::endl;
+   std::cout << "\nGEMV_BATCH" << std::endl;
    for(int i=1; i<=10; i++){  
       int N = i*N0;
-      auto t0 = tools::get_time();
-      test_xgemv_batch<Tm>(N, N, nbatch);
-      auto t1 = tools::get_time();
-      double dt = tools::get_duration(t1-t0);
-      std::cout << "i=" << i << " M,N=" << N << "," << N 
+      double dt = test_xgemv_batch<Tm>(N, N, nbatch);
+      std::cout << " i=" << i << " M,N=" << N << "," << N 
          << " nbatch=" << nbatch
          << " time=" << dt
          << " flops=" << nbatch*2*double(N)*N/dt
