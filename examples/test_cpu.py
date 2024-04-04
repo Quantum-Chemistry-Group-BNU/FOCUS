1f6e38889b362fa6c105d5669204c04b822bf40e
zhendongli <zhendongli2008@gmail.com>
2024-04-04 10:28:26 +0800

    add more tests

--- examples/test_cpu.py
+++ examples/test_cpu.py
@@ -1,7 +1,7 @@
 import os
 import test_utils
 
-mpiprefix = "mpirun -np 2 "
+mpiprefix = "mpirun -np 1 "
 os.environ['OMP_NUM_THREADS'] = "4"
 print('OMP_NUM_THREADS=',os.environ.get('OMP_NUM_THREADS'))
 
@@ -26,5 +26,6 @@ dirs = ['tests_cpu/0_h6_tns',
         'tests_cpu/5_h5_cNSz_hvec5',
         'tests_cpu/7_h6_cisolver',
         'tests_cpu/8_h6+_sadmrg',
+        'tests_cpu/9_h6+_sadmrg_cisolver',
         ]
 test_utils.test_run(dirs)
