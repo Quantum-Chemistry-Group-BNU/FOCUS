8c3500fd5365eec3c23e4b0434e4bad09a93a92d
zhendongli <zhendongli2008@gmail.com>
2024-08-22 15:34:22 +0800

    support schmidt values for iroot

--- src/ctns/ctns_entropy.h
+++ src/ctns/ctns_entropy.h
@@ -4,41 +4,64 @@
 #include "sweep_twodot_guess.h"
 
 namespace ctns{
-
+        
+   // r*R0*R1*R2 to C0*R1*R2 
    template <typename Qm, typename Tm>
-      std::vector<std::vector<double>> get_schmidt_values(const comb<Qm,Tm>& icomb,
-            const bool debug=false){
-
-         // generate sweep sequence
-         const int dmax = icomb.get_dmax();
-         auto sweep_seq = icomb.topo.get_mps_fsweeps();
-         const int nroots = 1;
-         const int dots = 2;
-
-         // initialization: generate cpsi
-         const bool singlet = false;
-         auto sym_state = icomb.get_qsym_state();
+      void init_cpsi_dot0(comb<Qm,Tm>& icomb,
+            const int iroot=-1,
+            const bool singlet=true){
          const auto& rindex = icomb.topo.rindex;
          const auto& site0 = icomb.sites[rindex.at(std::make_pair(0,0))]; // will be updated
+         const auto sym_state = icomb.get_qsym_state();
          const auto wf2 = get_boundary_coupling<Qm,Tm>(sym_state, singlet); // env*C[0]
-         auto icomb_tmp = icomb;
-         icomb_tmp.cpsi.resize(nroots);
-         for(int iroot=0; iroot<nroots; iroot++){
+         int nroots = (iroot==-1)? icomb.get_nroots() : 1;
+         icomb.cpsi.resize(nroots);
+         if(iroot == -1){
+            for(int iroot=0; iroot<nroots; iroot++){
+               // qt2(1,r): ->-*->-
+               auto qt2 = contract_qt2_qt2(wf2,icomb.rwfuns[iroot]);
+               // qt2(1,r)*site0(r,r0,n0) = qt3(1,r0,n0)[CRcouple]
+               auto qt3 = contract_qt3_qt2("l",site0,qt2);
+               // recouple to qt3(1,r0,n0)[LCcouple]
+               icomb.cpsi[iroot] = qt3.recouple_lc();
+            } // iroot
+         }else{
+            // get an MPS for a single state
+            if(iroot != 0) icomb.rwfuns[0] = std::move(icomb.rwfuns[iroot]);
+            icomb.rwfuns.resize(1);
             // qt2(1,r): ->-*->-
-            auto qt2 = contract_qt2_qt2(wf2,icomb.rwfuns[iroot]);
+            auto qt2 = contract_qt2_qt2(wf2,icomb.rwfuns[0]);
             // qt2(1,r)*site0(r,r0,n0) = qt3(1,r0,n0)[CRcouple]
             auto qt3 = contract_qt3_qt2("l",site0,qt2);
             // recouple to qt3(1,r0,n0)[LCcouple]
-            icomb_tmp.cpsi[iroot] = qt3.recouple_lc();
-         } // iroot
+            icomb.cpsi[0] = qt3.recouple_lc();
+         }
+      }
 
-         // start forward sweep
-         const int nphysical = icomb.get_nphysical();
+   template <typename Qm, typename Tm>
+      std::vector<std::vector<double>> get_schmidt_values(const comb<Qm,Tm>& icomb,
+            const int iroot=0,
+            const bool singlet=true,
+            const bool debug=false){
+         const bool ifab = Qm::ifabelian;
+
+         // generate cpsi for dot0 to init the sweep
+         auto icomb_tmp = icomb;
+         init_cpsi_dot0(icomb_tmp, iroot, singlet);
+
+         // holder for schdmidt values
+         const int nphysical = icomb_tmp.get_nphysical();
          std::vector<std::vector<double>> svalues(nphysical-1);
-         for(int ibond=0; ibond<sweep_seq.size(); ibond++){
-            const auto& dbond = sweep_seq[ibond];
-            auto tp0 = icomb.topo.get_type(dbond.p0);
-            auto tp1 = icomb.topo.get_type(dbond.p1);
+         
+         // generate forward sweep sequence
+         const int nroots = 1;
+         const auto& rindex = icomb_tmp.topo.rindex;
+         const int dmax = icomb_tmp.get_dmax();
+         auto fsweep_seq = icomb_tmp.topo.get_mps_fsweeps();
+         for(int ibond=0; ibond<fsweep_seq.size(); ibond++){
+            const auto& dbond = fsweep_seq[ibond];
+            auto tp0 = icomb_tmp.topo.get_type(dbond.p0);
+            auto tp1 = icomb_tmp.topo.get_type(dbond.p1);
             std::string superblock;
             if(dbond.forward){
                superblock = dbond.is_cturn()? "lr" : "lc1";
@@ -46,7 +69,8 @@ namespace ctns{
                superblock = dbond.is_cturn()? "c1c2" : "c2r";
             }
             if(debug){
-               std::cout << "\nibond=" << ibond << "/seqsize=" << sweep_seq.size()
+               const int dots = 2;
+               std::cout << "\nibond=" << ibond << "/seqsize=" << fsweep_seq.size()
                   << " dots=" << dots << " dbond=" << dbond
                   << std::endl;
             }
@@ -57,7 +81,10 @@ namespace ctns{
             const auto& qr  = site1.info.qcol;
             const auto& qc1 = cpsi0.info.qmid;
             const auto& qc2 = site1.info.qmid;
-            qtensor4<Qm::ifabelian,Tm> wf(sym_state, ql, qr, qc1, qc2);
+            const auto sym_state = icomb_tmp.get_qsym_state();
+            auto wfsym = get_qsym_state(Qm::isym, sym_state.ne(),
+                  (ifab? sym_state.tm() : sym_state.ts()), singlet);
+            qtensor4<ifab,Tm> wf(wfsym, ql, qr, qc1, qc2);
             size_t ndim = wf.size();
             if(debug){
                std::cout << "wf4(diml,dimr,dimc1,dimc2)=(" 
@@ -79,8 +106,8 @@ namespace ctns{
             twodot_guess_v0(icomb_tmp, dbond, ndim, nroots, wf, v0, debug);
             assert(v0.size() == ndim*nroots);
             // perform decimation
-            qtensor2<Qm::ifabelian,Tm> rot;
-            std::vector<qtensor2<Qm::ifabelian,Tm>> wfs2(nroots);
+            qtensor2<ifab,Tm> rot;
+            std::vector<qtensor2<ifab,Tm>> wfs2(nroots);
             for(int i=0; i<nroots; i++){
                wf.from_array(&v0[i*ndim]);
                auto wf2 = wf.merge_lc1_c2r();
@@ -88,7 +115,7 @@ namespace ctns{
             }
             const bool iftrunc = true;
             const double rdm_svd = 1.5;
-            const int alg_decim = 0;
+            const int alg_decim = 0; // serial version
             std::string fname;
             std::vector<double> sigs2full; 
             double dwt;
@@ -99,7 +126,7 @@ namespace ctns{
                   debug);
             svalues[ibond] = std::move(sigs2full);
             // propagate to the next site
-            linalg::matrix<Tm> vsol(ndim,nroots,v0.data());
+            linalg::matrix<Tm> vsol(ndim, nroots, v0.data());
             twodot_guess_psi(superblock, icomb_tmp, dbond, vsol, wf, rot);
             vsol.clear();
          } // ibond
@@ -147,16 +174,21 @@ namespace ctns{
          auto t0 = tools::get_time();
 
          // compute the Schmidt decomposition
-         auto svalues = get_schmidt_values(icomb);
+         const bool debug = false;
+         const bool singlet = true; // use singlet embedding by default
+         auto svalues = get_schmidt_values(icomb, iroot, singlet, debug);
 
          // compute the entropy
          std::cout << "von Neumann entropies across all bonds:" << std::endl;
+         double s_sum = 0.0;
          for(int i=0; i<svalues.size(); i++){
-            auto sval = renyi_entropy(svalues[i], 1.0);
+            auto s_val = renyi_entropy(svalues[i], 1.0); // von Neumann entropy
+            s_sum += s_val; 
             std::cout << " ibond=" << i 
-               << " SvN=" << std::setprecision(6) << sval 
+               << " SvN=" << std::setprecision(6) << s_val 
                << std::endl;
          }
+         std::cout << "sum of all = " << s_sum << std::endl;
 
          // save into file
          std::cout << "save schdmidt values into file" << std::endl;
