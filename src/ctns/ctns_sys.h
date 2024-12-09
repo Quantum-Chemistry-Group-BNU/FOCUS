fb93d0f6aca33e1d0fbca86c82b11254fd23c2d2
zhendongli <zhendongli2008@gmail.com>
2024-12-09 16:00:24 +0800

    cpu mem

--- src/ctns/ctns_sys.h
+++ src/ctns/ctns_sys.h
@@ -1,25 +1,11 @@
 #ifndef CTNS_SYS_H
 #define CTNS_SYS_H
 
-#include <unistd.h>
-#include <ios>
-#include <iostream>
-#include <fstream>
-#include <string>
-//
-#include <math.h>
-#include <sys/time.h>
-#include <sys/resource.h>
-
-#include <cstdlib>
-
-#include <iostream>
-#include <cstdlib>
-
 #ifdef __linux__
 #include <fstream>
 #include <sstream>
 #include <unistd.h>
+#include <malloc.h>  // For malloc_stats
 
 inline double getMemoryUsageLinux() {
     std::ifstream file("/proc/self/statm");
@@ -30,9 +16,24 @@ inline double getMemoryUsageLinux() {
         long pageSize = sysconf(_SC_PAGE_SIZE);  // Get the system's page size
         return residentPages * pageSize / std::pow(1024.0, 3);  // Convert to GB
     }
-
     return 0;
 }
+
+inline double getAvailableMemoryLinux() {
+    std::ifstream meminfo("/proc/meminfo");
+    std::string line;
+    long availableMemory = 0;
+
+    while (std::getline(meminfo, line)) {
+        if (line.find("MemAvailable") != std::string::npos) {
+            // The value is in kB, so we return it in GB
+            sscanf(line.c_str(), "MemAvailable: %ld kB", &availableMemory);
+            return availableMemory / std::pow(1024.0,2);  // Convert to GB
+        }
+    }
+    return 0;  // Return 0 if the value couldn't be found
+}
+
 #endif
 
 #ifdef __APPLE__
@@ -65,83 +66,12 @@ inline double getMemoryUsage() {
 
 namespace ctns{
 
-   extern size_t totalAllocatedMemory;
-
-   // attempts to read the system-dependent data for a process' virtual memory
-   // size and resident set size, and return the results in KB.
    inline void get_sys_status(){
-      using std::ios_base;
-      using std::ifstream;
-      using std::string;
-
-      // 'file' stat seems to give the most reliable results
-      ifstream stat_stream("/proc/self/stat",ios_base::in);
-
-      // dummy vars for leading entries in stat that we don't care about
-      string pid, comm, state, ppid, pgrp, session, tty_nr;
-      string tpgid, flags, minflt, cminflt, majflt, cmajflt;
-      string utime, stime, cutime, cstime, priority, Nice;
-      string O, itrealvalue, starttime;
-
-      // the two fields we want
-      unsigned long vsize;
-      long rss;
-
-      stat_stream >> pid >> comm >> state >> ppid >> pgrp >> session >> tty_nr
-         >> tpgid >> flags >> minflt >> cminflt >> majflt >> cmajflt
-         >> utime >> stime >> cutime >> cstime >> priority >> Nice
-         >> O >> itrealvalue >> starttime >> vsize >> rss; // don't care about the rest
-
-      stat_stream.close();
-
-      long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
-      double vm_usage     = vsize /(1024.0*1024.0);
-      double resident_set = rss * page_size_kb /(1024.0);
-      std::cout << std::scientific;
-      std::cout << "CPUmem: VM:" << vm_usage/1024.0 << "GB"   // Virtual Memory used
-         << " RSS:" << resident_set/1024.0 << "GB";   // Actual Memory used
-
-      // Timing
-      struct rusage usage;
-      getrusage(RUSAGE_SELF, &usage);
-      double user = usage.ru_utime.tv_sec + usage.ru_utime.tv_usec/1.e6; // user time used
-      double sys  = usage.ru_stime.tv_sec + usage.ru_stime.tv_usec/1.e6; // sys time used
-      std::cout << "  Timing: User:" << user << "S" 
-         << " Sys:" << sys << "S"
-         << " Total:" << user+sys << "S"
-         << std::endl;
-
-
-      std::ifstream file("/proc/self/status");
-      std::string line;
-      long memoryUsage = 0;
-
-      while (std::getline(file, line)) {
-         if (line.find("VmRSS") != std::string::npos) {  // Resident Set Size (RAM)
-            size_t pos = line.find_last_of(" \t");
-            memoryUsage = std::stol(line.substr(pos + 1));  // Get the value (in KB)
-            break;
-         }
-      }
-      std::cout << "memoryUsage=" << memoryUsage/1024.0 << "GB" << std::endl;
-
-      std::cout << "usage=" << usage.ru_maxrss/1024.0 << "GB" << std::endl;  // Memory in kilobytes
-
-      std::ifstream file2("/proc/self/statm");
-      long totalPages, residentPages;
-
-      if (file2) {
-         file >> totalPages >> residentPages;
-         long pageSize = sysconf(_SC_PAGE_SIZE);  // Get the system's page size
-         std::cout << "lzd memory usage (RSS in KB):"
-           << residentPages * pageSize / 1024
-           << " KB" << std::endl;
-      }
-
-      std::cout << "Current memory usage: " << getMemoryUsage() << " GB" << std::endl;
-      std::cout << totalAllocatedMemory << ":"
-         << totalAllocatedMemory/std::pow(1024.0,3) << "GB"
-         << std::endl;
+      std::cout << "CPUmem(GB):"
+        << std::setprecision(3)
+        << " used=" << getMemoryUsage() 
+        << " avail=" << getAvailableMemoryLinux() 
+        << std::endl;
    }
 
 } // ctns
