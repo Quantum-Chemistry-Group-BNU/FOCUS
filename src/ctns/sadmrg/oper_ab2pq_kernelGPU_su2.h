9b12a4d106b3a5ace0ee137214331594f94ca31e
zhendongli <zhendongli2008@gmail.com>
2024-12-17 14:34:58 +0800

    debug

--- src/ctns/sadmrg/oper_ab2pq_kernelGPU_su2.h
+++ src/ctns/sadmrg/oper_ab2pq_kernelGPU_su2.h
@@ -25,7 +25,7 @@ namespace ctns{
          const bool ifkr = Qm::ifkr;
          const int sorb = qops.sorb;
          assert(ifkr);
-         assert(alg_a2p >= 3);
+         assert(alg_a2p == 3);
 
          // determine the size of work space
          size_t maxsize = qops.size_ops('A');
@@ -101,42 +101,44 @@ namespace ctns{
 #endif
 
             // construct opP from opA, if opP is exist on the current process
-            if(qops2.num_ops('P') == 0) continue;
-            auto t0c = tools::get_time();
-            const auto& pmap = qops2.indexmap('P');
-            // Ppq = xpqsr*Asr
-            const auto& amap = qops_tmp.indexmap('M');
-            for(const auto& pr : amap){
-               const auto& symP = pr.first;
-               const auto& aindex = pr.second;
-               if(pmap.find(symP) == pmap.end()) continue;
-               int ts = symP.ts();
-               const auto& pindex = pmap.at(symP);
-               size_t opsize = qops2('P').at(pindex[0]).size();
-               if(opsize == 0) continue;
-               // construct coefficient matrix
-               int rows = aindex.size();
-               int cols = pindex.size();
-               auto cmat = get_A2Pmat_su2(aindex, pindex, ts, int2e);
-               // copy it to gpu
-               size_t gpumem_cmat = cmat.size()*sizeof(Tm);
-               Tm* cmat_gpu = (Tm*)GPUmem.allocate(gpumem_cmat);
-               GPUmem.to_gpu(cmat_gpu, cmat.data(), gpumem_cmat); 
-               // contract opP(dat,pq) = opCrs(dat,rs)*x(rs,pq)
-               const Tm alpha = 1.0, beta = 1.0; // accumulation from different processes
-               const Tm* ptr_opM_gpu = dev_work + qops_tmp._offset.at(std::make_pair('M',aindex[0]));
-               Tm* ptr_opP_gpu = qops2._dev_data + qops2._offset.at(std::make_pair('P',pindex[0]));
-               linalg::xgemm_gpu("N", "N", opsize, cols, rows, alpha,
-                     ptr_opM_gpu, opsize, cmat_gpu, rows, beta,
-                     ptr_opP_gpu, opsize);
-               GPUmem.deallocate(cmat_gpu, gpumem_cmat);
-            } // amap
-            auto t1c = tools::get_time();
-            double dtc = tools::get_duration(t1c-t0c);
-            tcomp += dtc;
-            taccum += dtc;
-            if(rank == 0) std::cout << "   compute opP from opA: dt=" << dtc
-               << " tcomp=" << tcomp << " taccum=" << taccum << std::endl;
+            if(qops2.num_ops('P') >= 0){
+               auto t0c = tools::get_time();
+               // Ppq = xpqsr*Asr
+               const auto& pmap = qops2.indexmap('P');
+               const auto& amap = qops_tmp.indexmap('M');
+               for(const auto& pr : amap){
+                  const auto& symP = pr.first;
+                  const auto& aindex = pr.second;
+                  if(pmap.find(symP) == pmap.end()) continue;
+                  int ts = symP.ts();
+                  const auto& pindex = pmap.at(symP);
+                  size_t opsize = qops2('P').at(pindex[0]).size();
+                  if(opsize == 0) continue;
+                  // construct coefficient matrix
+                  int rows = aindex.size();
+                  int cols = pindex.size();
+                  auto cmat = get_A2Pmat_su2(aindex, pindex, ts, int2e);
+                  // copy it to gpu
+                  size_t gpumem_cmat = cmat.size()*sizeof(Tm);
+                  Tm* cmat_gpu = (Tm*)GPUmem.allocate(gpumem_cmat);
+                  GPUmem.to_gpu(cmat_gpu, cmat.data(), gpumem_cmat); 
+                  // contract opP(dat,pq) = opCrs(dat,rs)*x(rs,pq)
+                  const Tm alpha = 1.0, beta = 1.0; // accumulation from different processes
+                  const Tm* ptr_opM_gpu = dev_work + qops_tmp._offset.at(std::make_pair('M',aindex[0]));
+                  Tm* ptr_opP_gpu = qops2._dev_data + qops2._offset.at(std::make_pair('P',pindex[0]));
+                  linalg::xgemm_gpu("N", "N", opsize, cols, rows, alpha,
+                        ptr_opM_gpu, opsize, cmat_gpu, rows, beta,
+                        ptr_opP_gpu, opsize);
+                  GPUmem.deallocate(cmat_gpu, gpumem_cmat);
+               } // amap
+               auto t1c = tools::get_time();
+               double dtc = tools::get_duration(t1c-t0c);
+               tcomp += dtc;
+               taccum += dtc;
+               if(rank == 0) std::cout << "   compute opP from opA: dt=" << dtc
+                  << " tcomp=" << tcomp << " taccum=" << taccum << std::endl;
+            } // p
+
          } // iproc
 
          // Clean up
@@ -154,7 +156,7 @@ namespace ctns{
       }
 
    template <typename Qm, typename Tm, std::enable_if_t<!Qm::ifabelian,int> = 0>
-      void oper_b2qGPU(const comb<Qm,Tm>& icomb,
+      void oper_b2qGPUtmp(const comb<Qm,Tm>& icomb,
             const integral::two_body<Tm>& int2e,
             const qoper_dict<Qm::ifabelian,Tm>& qops,
             qoper_dict<Qm::ifabelian,Tm>& qops2,
@@ -170,7 +172,7 @@ namespace ctns{
          const int sorb = qops.sorb;
          assert(ifkr);
          assert(qops.ifhermi);
-         assert(alg_b2q >= 3);
+         assert(alg_b2q == 3);
 
          // determine the size of work space
          size_t maxsize = qops.size_ops('B');
@@ -187,7 +189,7 @@ namespace ctns{
          // loop over rank
          for(int iproc=0; iproc<size; iproc++){
             auto bindex_iproc = oper_index_opB_dist(qops.cindex, qops.ifkr, qops.isym, size, iproc, qops.sorb); 
-            
+
             // broadcast {opBqr} for given sym from iproc
             if(bindex_iproc.size() > 0){
                auto t0i = tools::get_time();
@@ -382,6 +384,7 @@ namespace ctns{
                      << " tcomp=" << tcomp << " taccum=" << taccum << std::endl;
                } // q
             } // b
+
          } // iproc
 
          // Clean up
@@ -395,6 +398,183 @@ namespace ctns{
                << " T(init/copy/adjt/bcast/comp/rest)=" << tinit << "," << tcopy << ","
                << tadjt << "," << tcomm << "," << tcomp << "," << trest << " -----"  
                << std::endl;
+         }
+      }
+
+   template <typename Qm, typename Tm, std::enable_if_t<!Qm::ifabelian,int> = 0>
+      void oper_b2qGPU(const comb<Qm,Tm>& icomb,
+            const integral::two_body<Tm>& int2e,
+            const qoper_dict<Qm::ifabelian,Tm>& qops,
+            qoper_dict<Qm::ifabelian,Tm>& qops2,
+            const int alg_b2q){
+         int size = 1, rank = 0;
+#ifndef SERIAL
+         size = icomb.world.size();
+         rank = icomb.world.rank();
+#endif
+         auto t_start = tools::get_time();
+         double tinit = 0.0, tcopy = 0.0, tadjt = 0.0, tcomm = 0.0, tcomp = 0.0, taccum = 0.0;
+         const bool ifkr = Qm::ifkr;
+         const int sorb = qops.sorb;
+         assert(ifkr);
+         assert(qops.ifhermi);
+         assert(alg_b2q >= 3);
+
+         // loop over rank
+         icomb.world.barrier();
+         for(int iproc=0; iproc<size; iproc++){
+            auto bindex_iproc = oper_index_opB_dist(qops.cindex, qops.ifkr, qops.isym, size, iproc, qops.sorb);
+            if(bindex_iproc.size() == 0) continue;
+            // broadcast {opBqr} for given sym from iproc
+            auto t0i = tools::get_time();
+            qoper_dict<Qm::ifabelian,Tm> qops_tmp;
+            qops_tmp.sorb = qops.sorb;
+            qops_tmp.isym = qops.isym;
+            qops_tmp.ifkr = qops.ifkr;
+            qops_tmp.cindex = qops.cindex;
+            qops_tmp.krest = qops.krest;
+            qops_tmp.qbra = qops.qbra;
+            qops_tmp.qket = qops.qket;
+            qops_tmp.oplist = "BN";
+            qops_tmp.mpisize = size;
+            qops_tmp.mpirank = iproc; // not rank
+            qops_tmp.ifdist2 = true;
+            qops_tmp.init();
+            if(qops_tmp.size() == 0) continue;
+            // allocate space
+            qops_tmp.allocate_gpu();
+            icomb.world.barrier();
+            auto t1i = tools::get_time();
+            tinit += tools::get_duration(t1i-t0i);
+            taccum += tools::get_duration(t1i-t0i);
+            if(rank == 0){
+               std::cout << "iproc=" << iproc << std::endl;
+               qops_tmp.print("qops_tmp");
+               std::cout << "   init qops_tmp: t=" << tools::get_duration(t1i-t0i)
+                  << " tinit=" << tinit << " taccum=" << taccum << std::endl;
+
+            }
+
+            auto t0x = tools::get_time();
+            if(iproc == rank){
+               linalg::xcopy_gpu(qops.size_ops('B'), qops.ptr_ops_gpu('B'), qops_tmp.ptr_ops_gpu('B'));
+
+            }
+            icomb.world.barrier();
+            auto t1x = tools::get_time();
+            tcopy += tools::get_duration(t1x-t0x);
+            taccum += tools::get_duration(t1x-t0x);
+            if(rank == 0) std::cout << "   copy opB to qops_tmps:"
+               << " t=" <<  tools::get_duration(t1x-t0x)
+                  << " tcopy=" << tcopy << " taccum=" << taccum << std::endl;
+
+#ifndef SERIAL
+            auto t0b = tools::get_time();
+            if(size > 1){
+               nccl_comm.broadcast(qops_tmp.ptr_ops_gpu('B'), qops_tmp.size_ops('B'), iproc);
+
+            }
+            icomb.world.barrier();
+            auto t1b = tools::get_time();
+            double tbcast = tools::get_duration(t1b-t0b);
+            tcomm += tbcast;
+            taccum += tbcast;
+            if(rank == 0){
+               size_t data_size = qops_tmp.size_ops('B');
+               std::cout << "   bcast: size(opB)=" << data_size << ":" << tools::sizeGB<Tm>(data_size) << "GB"
+                  << " t(bcast)=" << tbcast << " speed=" << tools::sizeGB<Tm>(data_size)/tbcast << "GB/s"
+                  << " tcomm=" << tcomm << " taccum=" << taccum
+                  << std::endl;
+
+            }
+#endif
+
+            auto t0y = tools::get_time();
+            // opB to opB.H() on GPU
+            batchedHermitianConjugateGPU(qops_tmp, 'B', qops_tmp, 'N', true, qops_tmp._dev_data, qops_tmp._dev_data);
+            auto t1y = tools::get_time();
+            tadjt += tools::get_duration(t1y-t0y);
+            taccum += tools::get_duration(t1y-t0y);
+            if(rank == 0) std::cout << "   from opB to opB.H(): size=" << bindex_iproc.size()
+               << " t=" <<  tools::get_duration(t1y-t0y)
+                  << " tadjt=" << tadjt << " taccum=" << taccum << std::endl;
+
+            // only perform calculation if opQ is exist on the current process
+            if(qops2.num_ops('Q') == 0) continue;
+            auto t0z = tools::get_time();
+            const auto& qmap = qops2.indexmap('Q');
+            // Qps = wqr*vpqsr*Bqr
+            const auto& bmap = qops_tmp.indexmap('B');
+            for(const auto& pr : bmap){
+               const auto& symQ = pr.first;
+               const auto& bindex = pr.second;
+               if(qmap.find(symQ) == qmap.end()) continue;
+               int ts = symQ.ts();
+               const auto& qindex = qmap.at(symQ);
+               size_t opsize = qops2('Q').at(qindex[0]).size();
+               if(opsize == 0) continue;
+               // construct coefficient matrix
+               int rows = bindex.size();
+               int cols = qindex.size();
+               auto cmat = get_B2Qmat_su2(bindex, qindex, ts, int2e, false);
+               // copy it to gpu
+               size_t gpumem_cmat = cmat.size()*sizeof(Tm);
+               Tm* cmat_gpu = (Tm*)GPUmem.allocate(gpumem_cmat);
+               GPUmem.to_gpu(cmat_gpu, cmat.data(), gpumem_cmat);
+               // contract opQ(dat,ps) = opBqr(dat,qr)*x(ps,qr)
+               const Tm alpha = 1.0, beta = 1.0; // accumulation from different processes
+               const Tm* ptr_opB_gpu = qops_tmp._dev_data + qops_tmp._offset.at(std::make_pair('B',bindex[0]));
+               Tm* ptr_opQ_gpu = qops2._dev_data + qops2._offset.at(std::make_pair('Q',qindex[0]));
+               linalg::xgemm_gpu("N", "N", opsize, cols, rows, alpha,
+                     ptr_opB_gpu, opsize, cmat_gpu, rows, beta,
+                     ptr_opQ_gpu, opsize);
+               GPUmem.deallocate(cmat_gpu, gpumem_cmat);
+
+            } // bmap
+            // Qps = (-1)^k*wqr*vprsq*Bqr.H()
+            const auto& nmap = qops_tmp.indexmap('N');
+            for(const auto& pr : nmap){
+               const auto& symQ = pr.first;
+               const auto& bindex = pr.second;
+               if(qmap.find(symQ) == qmap.end()) continue;
+               int ts = symQ.ts();
+               const auto& qindex = qmap.at(symQ);
+               size_t opsize = qops2('Q').at(qindex[0]).size();
+               if(opsize == 0) continue;
+               // construct coefficient matrix
+               int rows = bindex.size();
+               int cols = qindex.size();
+               auto cmat = get_B2Qmat_su2(bindex, qindex, ts, int2e, true);
+               // copy it to gpu
+               size_t gpumem_cmat = cmat.size()*sizeof(Tm);
+               Tm* cmat_gpu = (Tm*)GPUmem.allocate(gpumem_cmat);
+               GPUmem.to_gpu(cmat_gpu, cmat.data(), gpumem_cmat);
+               // contract opQ(dat,ps) = opBqr(dat,qr)*x(ps,qr)
+               const Tm alpha = 1.0, beta = 1.0; // accumulation from different processes
+               const Tm* ptr_opN_gpu = qops_tmp._dev_data + qops_tmp._offset.at(std::make_pair('N',bindex[0]));
+               Tm* ptr_opQ_gpu = qops2._dev_data + qops2._offset.at(std::make_pair('Q',qindex[0]));
+               linalg::xgemm_gpu("N", "N", opsize, cols, rows, alpha,
+                     ptr_opN_gpu, opsize, cmat_gpu, rows, beta,
+                     ptr_opQ_gpu, opsize);
+               GPUmem.deallocate(cmat_gpu, gpumem_cmat);
+
+            } // nmap
+            auto t1z = tools::get_time();
+            tcomp += tools::get_duration(t1z-t0z);
+            taccum += tools::get_duration(t1z-t0z);
+            if(rank == 0) std::cout << "   compute opQ from opB: t=" << tools::get_duration(t1z-t0z)
+               << " tcomp=" << tcomp << " taccum=" << taccum << std::endl;
+         } // iproc
+
+         auto t_end = tools::get_time();
+         if(rank == 0){
+            double t_tot = tools::get_duration(t_end-t_start);
+            double trest = t_tot - tinit - tcopy - tadjt - tcomm - tcomp;
+            std::cout << "----- TIMING FOR oper_b2qGPU(su2) : " << t_tot << " S"
+               << " T(init/copy/adjt/bcast/comp/rest)=" << tinit << "," << tcopy << ","
+               << tadjt << "," << tcomm << "," << tcomp << "," << trest << " -----"
+               << std::endl;
+
          }
       }
 
