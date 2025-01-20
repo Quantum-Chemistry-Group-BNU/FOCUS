c39804fb8ea7f20e01569554880aa52cb20988f8
zhendongli <zhendongli2008@gmail.com>
2025-01-20 19:06:08 +0800

    add script for spinAnalysis

---
+++ pyutils/spinAnalysis/rdmutils.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+def get_twordm_spinorbital_matrix(fname='rdm2mps.0.0.txt'):
+    mat = np.loadtxt(fname)
+    return mat
+
+def get_twordm_spinorbital_tensor(norb,fname='rdm2mps.0.0.txt'):
+    mat = np.loadtxt(fname)
+    sorb = 2*norb
+    twordm = np.zeros((sorb,sorb,sorb,sorb)) # rdm2[i,j,k,l] = <i^+j^+kl> (i>j,l>k in matrix)
+    for i in range(sorb):
+        for j in range(sorb):
+            if i == j: continue
+            if i>j:
+                ij = i*(i-1)//2+j
+                sgnij = 1
+            else:
+                ij = j*(j-1)//2+i
+                sgnij = -1
+            for k in range(sorb):
+                for l in range(sorb):
+                    if k == l: continue
+                    if l>k:
+                        lk = l*(l-1)//2+k
+                        sgnlk = 1
+                    else:
+                        lk = k*(k-1)//2+l
+                        sgnlk = -1
+                    twordm[i,j,k,l] = mat[ij,lk]*sgnij*sgnlk
+    return twordm
+
+# rdm2[i,j,k,l] = <is1^+js2^+ks2ls1> = G[aaaa] + G[abba] + G[baab] + G[bbbb]
+def get_twordm_spatialorbital_tensor(norb,fname='rdm2mps.0.0.txt'):
+    twordm_spin = get_twordm_spinorbital_tensor(norb,fname)
+    twordm = twordm_spin[::2,::2,::2,::2] \
+            + twordm_spin[::2,1::2,1::2,::2] \
+            + twordm_spin[1::2,::2,::2,1::2] \
+            + twordm_spin[1::2,1::2,1::2,1::2]
+    return twordm
+
+# interface to oodmrg
+def twordm_backtransform(twordm_spatial,urot):
+    urot2 = urot.T.copy() # back to original basis
+    tmp = np.einsum('ijkl,ip->pjkl',twordm_spatial,urot2)
+    tmp = np.einsum('pjkl,jq->pqkl',tmp,urot2)
+    tmp = np.einsum('pqkl,kr->pqrl',tmp,urot2)
+    tmp = np.einsum('pqrl,ls->pqrs',tmp,urot2)
+    return tmp
---
+++ pyutils/spinAnalysis/spinAnalysis.py
@@ -0,0 +1,75 @@
+import numpy as np
+import spinTools
+
+def spinAnalysis(twordm_spatial_lmo,groups,k,n,s):
+    print('\n[spinAnalysis]: k,n,s=',k,n,s)
+    twordm = twordm_spatial_lmo
+    onerdm = np.einsum('ijjl->il',twordm)/(n-1)
+    print('<n>=',np.trace(onerdm))
+    print('<s2>=',spinTools.local_spinsquare(onerdm,twordm))
+    
+    ng = len(groups)
+
+    # <Si^2>
+    si2 = np.zeros(ng)
+    for i,ig in enumerate(groups):
+        rdm1 = onerdm[np.ix_(ig,ig)]
+        rdm2 = twordm[np.ix_(ig,ig,ig,ig)]
+        sij = spinTools.local_spinsquare(rdm1,rdm2)
+        si2[i] = sij
+    for i in range(len(groups)):
+        print('igroup=',i,'s2exp=',si2[i],'seff=',spinTools.from_spinsquare_to_spin(si2[i]))
+
+    # <Si*Sj>
+    sisj = np.zeros((ng,ng))
+    for i,ig in enumerate(groups):
+        for j,jg in enumerate(groups):
+            if i>=j: continue 
+            bas1 = ig+jg
+            rdm1 = onerdm[np.ix_(bas1,bas1)].copy()
+            rdm2 = twordm[np.ix_(bas1,bas1,bas1,bas1)].copy()
+            sij = spinTools.sisj(rdm1,rdm2,len(ig),len(jg))
+            sisj[i,j] = sij
+            sisj[j,i] = sij
+        sisj[i,i] = si2[i] 
+    print('s2sum=',np.sum(sisj))
+    print(sisj)
+
+    # <Sz>
+    srdm = spinTools.szHS(onerdm,twordm,n,s)
+    print('tr(Sz)=',np.trace(srdm))
+    sz = np.zeros(ng)
+    for i,ig in enumerate(groups):
+        rdm1 = srdm[np.ix_(ig,ig)]
+        sz[i] = np.trace(rdm1)
+    print('sz=',sz,np.sum(sz))
+
+    # <Ne>
+    ne = np.zeros(ng)
+    for i,ig in enumerate(groups):
+        rdm1 = onerdm[np.ix_(ig,ig)]
+        ne[i] = np.trace(rdm1)
+    print('ne=',ne,np.sum(ne))
+    return sisj
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+def genSpinSpinPlot(sisj,fname='fe4s4.pdf'):
+    # Plot the weighted
+    ischeme = 'nearest'
+    fig, axes = plt.subplots(1, 1, figsize=(12, 6),
+    			 subplot_kw={'xticks': [], 'yticks': []})
+    fig.subplots_adjust(hspace=0.3, wspace=0.05)
+    clmap = cm.coolwarm
+    im = axes.imshow(sisj,interpolation=ischeme,cmap=clmap)
+    #axes[1].imshow(sisjList[1],interpolation=ischeme)
+    #axes[2].imshow(sisjList[2],interpolation=ischeme)
+    #axes[3].imshow(sisjList[3],interpolation=ischeme)
+    #>fig.subplots_adjust(right=0.85)
+    #>cbar_ax = fig.add_axes([0.88, 0.15, 0.03, 0.7])
+    #>fig.colorbar(im, cax=cbar_ax)
+    plt.savefig(fname)
+    plt.show()
+    print('saved to fname=',fname)
+    return 0
